5310b7b5fe9162b3a6dcff2bf9f9666c63e51558
Ruslan Kornev <oganer@gmail.com>
2019-08-11 09:47:24 +0300

    Extracts Authenticatable concern and adds relations for Doorkeeper

---
+++ app/models/concerns/authenticatable.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# Used in User model
+module Authenticatable
+  extend ActiveSupport::Concern
+
+  included do
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
+
+    has_many :access_grants,
+             class_name: 'Doorkeeper::AccessGrant',
+             foreign_key: :resource_owner_id,
+             dependent: :delete_all # or :destroy if you need callbacks
+
+    has_many :access_tokens,
+             class_name: 'Doorkeeper::AccessToken',
+             foreign_key: :resource_owner_id,
+             dependent: :delete_all # or :destroy if you need callbacks
+  end
+end
