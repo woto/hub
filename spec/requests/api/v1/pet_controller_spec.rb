b8f99d402ca83b0f266c8805d136d729ff4d4e1e
Ruslan Kornev <oganer@gmail.com>
2019-09-12 23:50:22 +0300

    Adds test for authentication with headers

--- spec/requests/api/v1/pet_controller_spec.rb
+++ spec/requests/api/v1/pet_controller_spec.rb
@@ -1,16 +1,32 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'rails_helper'
 
 describe Api::V1::PetController, type: :request do
-  def make
-    post '/api/v1/pet', params: { access_token: token.token }
-  end
-
   let!(:token) { create(:oauth_access_tokens) }
 
-  it do
-    make
-    expect(response).to have_http_status(:ok)
+  context 'when authenticates with params' do
+    def make
+      post '/api/v1/pet', params: { access_token: token.token }
+    end
+
+    it do
+      make
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context 'when authenticates with headers' do
+    def make
+      headers = {
+        'Authorization' => "Bearer #{token.token}"
+      }
+      post '/api/v1/pet', headers: headers
+    end
+
+    it 'authenticates with headers' do
+      make
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
