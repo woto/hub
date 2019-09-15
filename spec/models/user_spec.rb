b1a655d11793bcdd87a14d79aa8f7a77a9946b84
Ruslan Kornev <oganer@gmail.com>
2019-09-15 09:07:23 +0300

    Adds changing email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with confirmed email' do
+    let(:user) { create(:user) }
+
+    it { expect(user).to be_confirmed }
+    it 'receives confirmation email when changes his email' do
+      # TODO: rewrite with dynamic host
+      expect do
+        user.update(email: user.email.prepend('changed_'))
+      end.to change(ActionMailer::Base.deliveries, :count).by(1)
+    end
+  end
 end
