6f4ef80a53c2457a90a880e8131a4f38567f80a8
Ruslan Kornev <oganer@gmail.com>
2019-09-28 23:57:10 +0300

    Adds first puppeteer translations tests

---
+++ docker/puppeteer/tests/shared/Translations.spec.js
@@ -0,0 +1,29 @@
+/* eslint-disable mocha/no-mocha-arrows */
+/* eslint-disable no-console */
+/* eslint-disable prefer-arrow-callback */
+
+// const assert = require('assert');
+const { expect } = require('chai');
+
+const shared = require('../support/puppeteer');
+
+describe('Translations', function() {
+
+  this.timeout(5000);
+  shared.preparePuppeteer.call(this);
+
+  it('Expects project title in russian', async () => {
+    await this.page.goto('https://ru.nv6.ru');
+    await this.page.waitForSelector('[jid="project-title"]');
+    const text = await this.page.$eval('[jid="project-title"]', el => el.textContent);
+    expect(text).to.equal('Проект nv6.ru');
+  });
+
+  it('Expects project title in english', async () => {
+    await this.page.goto('https://en.nv6.ru');
+    await this.page.waitForSelector('[jid="project-title"]');
+    const text = await this.page.$eval('[jid="project-title"]', el => el.textContent);
+    expect(text).to.equal('Project nv6.ru');
+  });  
+
+});
