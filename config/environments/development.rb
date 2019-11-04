71593ef2f19fe5c4c750ab22a20eaaba6e324b27
Ruslan Kornev <oganer@gmail.com>
2019-11-05 02:31:51 +0300

    Adds and sets up mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,4 +65,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'mailcatcher',
+    :port => 1025,
+  }
 end
