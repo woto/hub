dd80ebb5058e786fd2c27004731f12fdfe135def
Ruslan Kornev <oganer@gmail.com>
2020-06-27 15:24:43 +0300

    gems

--- spec/system/dashboard_spec.rb
+++ spec/system/dashboard_spec.rb
@@ -1,14 +1,18 @@
 require "rails_helper"
 
-RSpec.describe "Widget management", :type => :system do
+RSpec.describe "Widget management", type: :system do
   before do
-    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   end
 
   it "enables me to create widgets" do
     visit "/dashboard"
     click_link "Language"
-
     expect(page).to have_text("English")
+  end
+
+  it 'shows login link' do
+    visit "/dashboard"
+    expect(page).to have_text('Login')
   end
 end
