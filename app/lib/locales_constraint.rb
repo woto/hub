c849516944c803911cfe159ebb0ac84778537749
Ruslan Kornev <oganer@gmail.com>
2020-06-27 23:14:59 +0300

    tmp

---
+++ app/lib/locales_constraint.rb
@@ -0,0 +1,10 @@
+class LocalesConstraint
+  def matches?(request)
+    case request.params['locale']
+    when nil
+      true
+    when *I18n.available_locales.map(&:to_s)
+      true
+    end
+  end
+end
