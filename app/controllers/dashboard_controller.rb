42c9220a0d8cab10586fcb007be66c8629097692
Ruslan Kornev <oganer@gmail.com>
2020-06-27 22:58:14 +0300

    tmp

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class DashboardController < ApplicationController
+  # skip_after_action :verify_policy_scoped
+  layout 'dashboard'
+  skip_before_action :authenticate_user!
+
+  def index; end
+end
