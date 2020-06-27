da896c5075b43b59cceaa9a034b2d81675a82a0c
Ruslan Kornev <oganer@gmail.com>
2020-06-27 10:24:00 +0300

    tmp

--- app/serializers/avatar_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AvatarSerializer
-  include FastJsonapi::ObjectSerializer
-
-  attribute :url do |object|
-    Rails.application.routes.url_helpers.rails_blob_path(object, only_path: true)
-  end
-end
