458d0582e873d52ccfd86380d5ca93b3f541e774
Ruslan Kornev <oganer@gmail.com>
2020-06-28 15:10:14 +0300

    we a arrived?

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by ENV.fetch('CAPYBARA_DRIVER', :selenium).to_sym,
-              using: ENV.fetch('CAPYBARA_BROWSER', :headless_chrome).to_sym,
+    driven_by ENV.fetch('CAPYBARA_DRIVER').to_sym,
+              using: ENV.fetch('CAPYBARA_BROWSER').to_sym,
               screen_size: [1400, 1400]
   end
 end
