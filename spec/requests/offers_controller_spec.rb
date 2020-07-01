84391f0d8cb6d46214b690fb9318f4a40ceee6e9
Ruslan Kornev <oganer@gmail.com>
2020-07-01 19:38:52 +0300

    to commit

--- spec/requests/offers_controller_spec.rb
+++ spec/requests/offers_controller_spec.rb
@@ -6,9 +6,14 @@ describe OffersController do
     Staff::Seeder::Elastic.pagination
   end
 
-  xit 'Is simply works :)' do
+  it 'Is simply works :)' do
     get '/feeds/zero_my_index_name/offers?page=1&q=one_my_offer_name'
-    assert_select 'h1[jid="name"]', count: 1, text: '[one] [my_offer_name] one_my_offer_name'
+    assert_select '[jid="name"]', count: 1, text: '[one] [my_offer_name] one_my_offer_name'
+  end
+
+  it 'Is simple works too :)' do
+    get '/offers?page=1'
+    assert_select '[jid="name"]', count: 10
   end
 
   context 'without feed' do
