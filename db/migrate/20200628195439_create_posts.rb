68fc70d364da25d13260613256331fed66610d6f
Ruslan Kornev <oganer@gmail.com>
2020-07-07 03:02:42 +0300

    post migrations

---
+++ db/migrate/20200628195439_create_posts.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.string :status_state, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
