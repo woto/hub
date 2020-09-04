889ceb9a94b2773a6a4cf50f0201f3ab12f6fe6a
Ruslan Kornev <oganer@gmail.com>
2020-09-05 00:37:51 +0300

    analyzer

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,18 @@
 const { environment } = require('@rails/webpacker')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 const webpack = require('webpack')
+
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    Popper: ['popper.js', 'default'],
+    bootstrap: ['bootstrap']
 }))
+
+environment.plugins.append(
+  'BundleAnalyzer',
+  new BundleAnalyzerPlugin()
+)
 
 module.exports = environment;
