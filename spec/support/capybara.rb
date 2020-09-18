4697b7a7067a05476e870a57aa004c47751dec28
Ruslan Kornev <oganer@gmail.com>
2020-09-19 02:09:48 +0300

    capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,31 @@
 # frozen_string_literal: true
 
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by ENV.fetch('CAPYBARA_DRIVER').to_sym,
-              using: ENV.fetch('CAPYBARA_BROWSER').to_sym,
-              screen_size: [1400, 1400]
+[
+  { name: :mobile, resolution: [375, 812] },
+  { name: :desktop, resolution: [1280, 1024] }
+].each do
+  _1 in { name:, resolution: }
+  Capybara.register_driver name do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_option('prefs', 'intl.accept_languages' => 'en-US')
+    if ActiveModel::Type::Boolean.new.cast(ENV.fetch('CAPYBARA_HEADLESS'))
+      options.headless!
+    end
+
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      # desired_capabilities: capabilities,
+      # http_client: client,
+      options: options
+    ).tap do |driver|
+      driver.browser.manage.window.size = Selenium::WebDriver::Dimension.new(*resolution)
+    end
+  end
+
+  RSpec.configure do |config|
+    config.before(:each, type: :system, browser: neme) do
+      driven_by name
+    end
   end
 end
