4bea0edcdc9cbb9c5d6318834cddd33d5cf992dc
Ruslan Kornev <oganer@gmail.com>
2020-09-20 00:55:23 +0300

    pagination rules

--- app/lib/pagination_rules.rb
+++ app/lib/pagination_rules.rb
@@ -4,18 +4,21 @@ class PaginationRules
   MAX_PER = 100
   DEFAULT_PER = 20
   PER_VARIANTS = [5, 10, DEFAULT_PER, 50, MAX_PER].freeze
+  attr_accessor :per, :page, :default_per, :max_per, :per_variants
 
-  def self.call(request, default_per = DEFAULT_PER, max_per = MAX_PER, per_variants = PER_VARIANTS)
+  def initialize(request, default_per = DEFAULT_PER, max_per = MAX_PER, per_variants = PER_VARIANTS)
+    @default_per = default_per
+    @max_per = max_per
+    @per_variants = per_variants
+
     # Range: (1..)
-    page = ((request.params[:page]&.to_i || 1))
-    page = 1 if page < 1
+    @page = ((request.params[:page]&.to_i || 1))
+    @page = 1 if @page < 1
 
     # Range: (per|default_per|DEFAULT_PER..max_per)
-    per = request.params[:per]&.to_i || Session::Tables::(request).per || default_per
-    per = max_per if per > max_per
-    per = DEFAULT_PER unless PER_VARIANTS.include?(per)
-    Session::Tables::(request).per = per
-
-    [page, per]
+    @per = request.params[:per]&.to_i || Session::Tables::(request).per || default_per
+    @per = max_per if @per > max_per
+    @per = @default_per unless @per_variants.include?(@per)
+    Session::Tables::(request).per = @per
   end
 end
