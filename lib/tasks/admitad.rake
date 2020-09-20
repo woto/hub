9a3fa883b5aae01a39dc5a1cfdf3b7833e4eb70f
Ruslan Kornev <oganer@gmail.com>
2020-09-20 19:57:20 +0300

    admitad rake

--- lib/tasks/admitad.rake
+++ lib/tasks/admitad.rake
@@ -1,19 +1,14 @@
+# frozen_string_literal: true
+
 Rails.logger = Logger.new(STDOUT)
 
-namespace :admitad do
-
-  namespace :campaigns do
-    desc "Sync camapaigns list"
+namespace :hub do
+  namespace :admitad do
+    desc 'Sync camapaigns list'
     task sync: :environment do
-      context = Ext::Admitad::Api::Token::Restore.call
-      context = Ext::Admitad::Api::Token::Retrive.call if context.failure?
-      Ext::Admitad::Api::Campaigns::Sync.call(context)
-      Rails.logger.debug("Finished")
-    end
-
-    desc "Download and process feeds"
-    task process: :environment do
-      Ext::Admitad::Api::Campaigns::Processing.call
+      context = Networks::Admitad::Token::Restore.call
+      context = Networks::Admitad::Token::Retrieve.call if context.failure?
+      Networks::Admitad::Sync.call(context)
     end
   end
 end
--- spec/tasks/admitad_spec.rb
+++ spec/tasks/admitad_spec.rb
@@ -2,6 +2,26 @@
 
 require 'rails_helper'
 
-describe "hub:admitad:sync" do
-  pending
+describe 'hub:admitad:sync' do
+  context 'when unable to find token in cache' do
+    let(:context) { instance_double(Interactor::Context, failure?: true) }
+
+    it 'retrieves new token' do
+      expect(Networks::Admitad::Token::Restore).to receive(:call).and_return(context)
+      expect(Networks::Admitad::Token::Retrieve).to receive(:call)
+      expect(Networks::Admitad::Sync).to receive(:call)
+      subject.execute
+    end
+  end
+
+  context 'when token found in cache' do
+    let(:context) { instance_double(Interactor::Context, failure?: false) }
+
+    it 'does not tries to retrieve it' do
+      expect(Networks::Admitad::Token::Restore).to receive(:call).and_return(context)
+      expect(Networks::Admitad::Token::Retrieve).not_to receive(:call)
+      expect(Networks::Admitad::Sync).to receive(:call)
+      subject.execute
+    end
+  end
 end
