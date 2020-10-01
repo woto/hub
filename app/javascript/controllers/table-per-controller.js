a27accb104eb54cb3fe468e4bfd46259b8e3c13b
Ruslan Kornev <oganer@gmail.com>
2020-10-02 01:56:12 +0300

    Fixes selected option when navigating back

--- app/javascript/controllers/table-per-controller.js
+++ app/javascript/controllers/table-per-controller.js
@@ -1,6 +1,10 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+    connect() {
+        this.element.value = this.data.get('select-value');
+    }
+
     visit(event) {
         let url = new URL(window.location.href);
         let params = new window.URLSearchParams(window.location.search);
