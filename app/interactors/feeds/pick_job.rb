d34cc71354e42a8ffdb34ee877c9a445fd7c1ab0
Ruslan Kornev <oganer@gmail.com>
2020-10-03 21:51:15 +0300

    Feeds::PickJob

---
+++ app/interactors/feeds/pick_job.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Feeds::PickJob
+  include ApplicationInteractor
+
+  def call
+    ActiveRecord::Base.transaction do
+      feed = Feeds::QueueQuery.call.lock.first
+      if feed.present?
+        feed.update!(
+          operation: 'pick job',
+          locked_by_pid: Process.pid,
+          attempt_uuid: SecureRandom.uuid,
+          processing_started_at: Time.current,
+          error_class: nil,
+          error_text: nil,
+          priority: 0
+        )
+        Rails.logger.info(feed)
+      else
+        Rails.logger.info('No suitable jobs were found')
+        exit
+      end
+
+      context.feed = feed
+    end
+  end
+end
---
+++ spec/interactors/feeds/pick_job_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Feeds::PickJob do
+  skip 'TODO! highly important'
+end
