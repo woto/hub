5627d5a7806ce5d0b4509fc5d7bf02109a45903e
Ruslan Kornev <oganer@gmail.com>
2020-10-05 02:39:58 +0300

    application_policy.rb

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,21 +1,55 @@
 # frozen_string_literal: true
 
 class ApplicationPolicy
-  def initialize(user, record)
-    raise Pundit::NotAuthorizedError, 'must be logged in' unless user
+  attr_reader :user, :record
 
-    @user   = user
+  def initialize(user, record)
+    # raise Pundit::NotAuthorizedError, 'must be logged in' unless user
+
+    @user = user
     @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
   end
 
   class Scope
     attr_reader :user, :scope
 
     def initialize(user, scope)
-      raise Pundit::NotAuthorizedError, 'must be logged in' unless user
+      # raise Pundit::NotAuthorizedError, 'must be logged in' unless user
 
       @user = user
       @scope = scope
+    end
+
+    def resolve
+      scope.all
     end
   end
 end
