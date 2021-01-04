33bab78bb2714851962118b26588167bfb6c393d
Ruslan <oganer@gmail.com>
2021-01-04 04:07:32 +0300

    wip locales

---
+++ config/locale.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+# NOTE: this is slightly modified version of
+# https://github.com/rack/rack-contrib/blob/master/lib/rack/contrib/locale.rb
+#
+require 'i18n'
+
+class Locale
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    locale_to_restore = I18n.locale
+
+    locale = locale_from_domain(env['HTTP_HOST'])
+    locale ||= locale_from_path(env['PATH_INFO'])
+    locale ||= locale_from_cookie(env['HTTP_COOKIE'])
+    locale ||= user_preferred_locale(env['HTTP_ACCEPT_LANGUAGE'])
+    locale ||= I18n.default_locale
+
+    locale_string = locale.to_s
+    status = nil
+    headers = nil
+    body = nil
+
+    I18n.with_locale(locale_string) do
+      status, headers, body = @app.call(env)
+    end
+
+    headers = Rack::Utils::HeaderHash.new(headers)
+    headers['Content-Language'] = locale_string unless headers['Content-Language']
+    Rack::Utils.set_cookie_header!(headers, 'locale', { value: locale_string, path: '/' })
+
+    [status, headers, body]
+  end
+
+  private
+
+  def locale_from_path(path)
+    locale = path.split('/').reject(&:blank?).first
+    locale if I18n.available_locales.find { |al| match?(al, locale) }
+  end
+
+  def locale_from_domain(host)
+    locale = host.sub(/:\d+$/, '').split('.').first
+    locale if I18n.available_locales.find { |al| match?(al, locale) }
+  end
+
+  def locale_from_cookie(cookie)
+    locale = Rack::Utils.parse_cookies_header(cookie)['locale']
+    locale if I18n.available_locales.find { |al| match?(al, locale) }
+  end
+
+  # Accept-Language header is covered mainly by RFC 7231
+  # https://tools.ietf.org/html/rfc7231
+  #
+  # Related sections:
+  #
+  # * https://tools.ietf.org/html/rfc7231#section-5.3.1
+  # * https://tools.ietf.org/html/rfc7231#section-5.3.5
+  # * https://tools.ietf.org/html/rfc4647#section-3.4
+  #
+  # There is an obsolete RFC 2616 (https://tools.ietf.org/html/rfc2616)
+  #
+  # Edge cases:
+  #
+  # * Value can be a comma separated list with optional whitespaces:
+  #   Accept-Language: da, en-gb;q=0.8, en;q=0.7
+  #
+  # * Quality value can contain optional whitespaces as well:
+  #   Accept-Language: ru-UA, ru; q=0.8, uk; q=0.6, en-US; q=0.4, en; q=0.2
+  #
+  # * Quality prefix 'q=' can be in upper case (Q=)
+  #
+  # * Ignore case when match locale with I18n available locales
+  #
+  def user_preferred_locale(header)
+    return if header.nil?
+
+    locales = header.gsub(/\s+/, '').split(',').map do |language_tag|
+      locale, quality = language_tag.split(/;q=/i)
+      quality = quality ? quality.to_f : 1.0
+      [locale, quality]
+    end.reject do |(locale, quality)|
+      locale == '*' || quality.zero?
+    end.sort_by do |(_, quality)|
+      quality
+    end.map(&:first)
+
+    return if locales.empty?
+
+    if I18n.enforce_available_locales
+      locale = locales.reverse.find { |locale| I18n.available_locales.any? { |al| match?(al, locale) } }
+      I18n.available_locales.find { |al| match?(al, locale) } if locale
+    else
+      locales.last
+    end
+  end
+
+  def match?(s1, s2)
+    s1.to_s.casecmp(s2.to_s).zero?
+  end
+end
--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -7,41 +7,41 @@ describe ApplicationController do
     context 'when subdomain' do
       it 'sets session locale' do
         host! 'ru.example.com'
-        expect(I18n).to receive(:with_locale).with('ru', any_args)
+        expect(I18n).to receive(:with_locale).with('ru').and_call_original
         get '/'
-        expect(session[:locale]).to eq('ru')
+        expect(cookies[:locale]).to eq('ru')
       end
     end
 
     context 'when locale in params' do
       it 'sets session locale' do
-        expect(I18n).to receive(:with_locale).with('pt', any_args)
+        expect(I18n).to receive(:with_locale).with('pt').and_call_original
         get '/pt'
-        expect(session[:locale]).to eq('pt')
+        expect(cookies[:locale]).to eq('pt')
       end
     end
 
     context 'when locale in session' do
       it 'preserves session locale' do
         get '/zh-CN'
-        expect(session[:locale]).to eq('zh-CN')
-        expect(I18n).to receive(:with_locale).with('zh-CN', any_args)
+        expect(cookies[:locale]).to eq('zh-CN')
+        expect(I18n).to receive(:with_locale).with('zh-CN').and_call_original
         get '/'
-        expect(session[:locale]).to eq('zh-CN')
+        expect(cookies[:locale]).to eq('zh-CN')
       end
     end
 
     context 'when locale is not available anywhere' do
       it 'uses default locale' do
-        expect(I18n).to receive(:with_locale).with('en', any_args)
+        expect(I18n).to receive(:with_locale).with('en').and_call_original
         get '/'
-        expect(session[:locale]).to eq('en')
+        expect(cookies[:locale]).to eq('en')
       end
     end
 
     describe 'Great Britain english locale' do
       specify do
-        expect(I18n).to receive(:with_locale).with('en-GB', any_args)
+        expect(I18n).to receive(:with_locale).with('en-GB').and_call_original
         get '/en-GB'
       end
     end
