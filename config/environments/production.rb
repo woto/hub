4cee527559504842d71c591ba64617a2c43ab44a
Ruslan <oganer@gmail.com>
2021-02-16 02:42:12 +0300

    feat: change available_locales in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: ENV['SMTP_USERNAME'],
@@ -124,4 +124,5 @@ Rails.application.configure do
   }
 
   config.action_mailbox.ingress = :sendgrid
+  config.i18n.available_locales = %w[en en-US ru]
 end
