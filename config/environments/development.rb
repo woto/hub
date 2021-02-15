6a920cee638ba925fa61ca172a05ce6d72b017b9
Ruslan <oganer@gmail.com>
2021-02-16 02:39:43 +0300

    feat: change development time zone to UTC

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
 
   # NOTE: specially changed to random time zone in order to not miss datetime conversions.
-  config.time_zone = 'Eastern Time (US & Canada)'
+  config.time_zone = 'UTC'
 
   config.i18n.available_locales = %w[en en-US ru]
 end
