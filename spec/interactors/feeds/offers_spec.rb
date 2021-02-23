9773ef01860cb4fad43414789d9eae0f815dd8f9
Ruslan <oganer@gmail.com>
2021-02-24 02:51:15 +0300

    test: move language test in correct place

--- spec/interactors/feeds/offers_spec.rb
+++ spec/interactors/feeds/offers_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 describe Feeds::Offers do
-  skip 'TODO! highly important'
+  describe 'Import::Offers::Language' do
+    let(:feed) { create(:feed) }
+
+    let(:doc) { Nokogiri::XML('<description>Отличный подарок для любителей венских вафель.</description>') }
+
+    it 'calls Import::Offers::Language.call with correct argument' do
+      expect(Import::Offers::Language).to receive(:call).with(
+        include('description' => [include('#' => include('Отличный подарок для любителей венских вафель.'))]))
+      instance = described_class.new(OpenStruct.new(feed: feed))
+      instance.append(doc)
+    end
+  end
 end
--- spec/interactors/feeds/parse_spec.rb
+++ spec/interactors/feeds/parse_spec.rb
@@ -8,20 +8,4 @@ describe Feeds::Parse do
   it 'does not raise error' do
     described_class.call(feed: feed)
   end
-
-  # TODO: better place for this code is interactors/feeds/offers_spec.rb
-  describe 'Import::Offers::Language' do
-    let(:feed) do
-      create(:feed, :with_attempt_uuid, xml_file_path: file_fixture('feeds/feed_with_different_languages.xml'))
-    end
-
-    it 'calls Import::Offers::Language.call twice' do
-      expect(Import::Offers::Language).to receive(:call).with(
-        include(
-          'description' => [include('#' => include('Отличный подарок для любителей венских вафель.'))]))
-      expect(Import::Offers::Language).to receive(:call).with(
-        include('description' => [include('#' => include('Ice cream maker Brand 3811'))]))
-      described_class.call(feed: feed)
-    end
-  end
 end
