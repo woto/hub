55eeef177159d46049ce14518f8e949d09073b47
Ruslan <oganer@gmail.com>
2021-02-25 22:47:11 +0300

    feat: shrink Feeds::Offers

--- app/interactors/feeds/offers.rb
+++ app/interactors/feeds/offers.rb
@@ -3,201 +3,31 @@
 module Feeds
   class Offers
     include ApplicationInteractor
-
-    SELF_NAME_KEY = '*'
-    HASH_BANG_KEY = '#'
-    CATEGORY_ID_KEY = :feed_category_id
-    CATEGORY_IDS_KEY = :feed_category_ids
-    ATTEMPT_UUID_KEY = :attempt_uuid
-    FAVORITE_IDS_KEY = :favorite_ids
-    WRONG_CATEGORY_ERROR = -1
-    MULTIPLE_CATEGORY_ERROR = -2
-    UNEXPECTED_ATTRIBUTES_KEY = :unexpected_attributes
-    UNEXPECTED_ATTRIBUTES_ERROR = -1
-    SKIPPED_TAGS = ['delivery-options'].freeze
-
-    INDEXED_AT = :indexed_at
+    attr_accessor :total_count, :batch_count
 
     def initialize(context)
       @offers = []
+      @batch_count = 0
+      @total_count = 0
+      @categories = context.feed.feed_categories_for_import
       super
     end
 
-    def append(doc)
-      @current = Hash.new { |hash, key| hash[key] = [] }
-      @current[SELF_NAME_KEY] = doc.to_h
-
-      # append attributes if any
-      doc.elements.each do |child|
-        next if child.name.in?(SKIPPED_TAGS)
-
-        @current[child.name] << hashify(child)
-      end
-
-      # patch aliexpress
-      if [74, 82, 83, 60, 68, 70, 84, 64, 69, 76, 80, 78, 90, 67, 63, 59, 66,
-          62, 61, 65, 72, 77, 75, 71, 73, 87, 85, 88, 81, 79, 89, 86, 58].include?(context.feed.id)
-        patch_aliexpress
-      end
-
-      # append our category
-      enrich_category
-
-      # append attempt_uuid
-      enrich_attempt_uuid
-
-      # set language in @current
-      Import::Offers::Language.call(@current)
-
-      # enrich indexed_at
-      enrich_indexed_at
-
-      # enrich favorite_ids
-      enrich_favorite_ids
-
-      @offers << @current
+    def append(doc, offer = Hash.new { |hash, key| hash[key] = [] })
+      Import::Offers::Hashify.call(offer, doc)
+      Import::Offers::Category.call(offer, context.feed, @categories)
+      Import::Offers::StandardAttributes.call(offer, context.feed)
+      Import::Offers::Language.call(offer)
+      Import::Offers::FavoriteIds.call(offer, context.feed.advertiser, context.feed)
+      @batch_count += 1
+      @total_count += 1
+      @offers << offer
     end
 
     def flush
-      return if @offers.empty?
-
-      client = Elasticsearch::Client.new Rails.application.config.elastic
-      res = client.bulk(
-        index: Elastic::IndexName.offers,
-        routing: context.feed.id,
-        body: @offers.map do |offer|
-          {
-            index: {
-              _id: "#{offer[SELF_NAME_KEY]['id']}-#{context.feed.id}",
-              data: offer.merge(feed_id: context.feed.id, advertiser_id: context.feed.advertiser.id)
-            }
-          }
-        end
-      )
-      raise Feeds::Process::ElasticResponseError, res['errors'] if res['errors']
-
+      Import::Offers::Flush.call(@offers, context.feed.advertiser, context.feed, client)
+      @batch_count = 0
       @offers = []
     end
-
-    def size
-      @offers.size
-    end
-
-    private
-
-    def hashify(child)
-      hsh = GlobalHelper.hashify(child)
-
-      if child.attributes.size > 2
-        Rails.logger.warn("Offer child tag '#{child.inspect}' has more than 2 attributes")
-        hsh[UNEXPECTED_ATTRIBUTES_KEY] = UNEXPECTED_ATTRIBUTES_ERROR
-      end
-
-      raise Feeds::Process::ElasticUnexpectedNestingError, child.to_s unless child.elements.empty?
-
-      # TODO: is it safe to use CDATA? What about XSS?
-      raise Feeds::Process::ElasticUnexpectedNestingError, child.to_s if child.children.reject(&:text?).size > 1
-
-      hsh
-    end
-
-    def patch_aliexpress
-      @current['name'] = @current.delete('title') if @current.key?('title')
-      @current['picture'] = @current.delete('image') if @current.key?('image')
-    end
-
-    def enrich_attempt_uuid
-      @current[ATTEMPT_UUID_KEY] = context.feed.attempt_uuid
-    end
-
-    def enrich_category
-      return unless @current.key?('categoryId')
-
-      category_value = @current['categoryId']
-
-      if category_value.size > 1
-        Rails.logger.warn('Offer has multiple categoryId')
-        @current[CATEGORY_ID_KEY] = MULTIPLE_CATEGORY_ERROR
-        @current[CATEGORY_IDS_KEY] = MULTIPLE_CATEGORY_ERROR
-      end
-
-      ext_category_id = category_value.first[HASH_BANG_KEY]
-      feed_category = FeedCategory.find_by(feed_id: context.feed.id, ext_id: ext_category_id)
-      if feed_category
-        if feed_category.children?
-          feed_category = feed_category
-                          .children
-                          .find_or_create_by!(feed_id: context.feed.id, ext_id: "#{ext_category_id}!") do |fc|
-            fc.attempt_uuid = context.feed.attempt_uuid
-          end
-        end
-        @current[CATEGORY_ID_KEY] = feed_category.id
-        @current[CATEGORY_IDS_KEY] = feed_category.path_ids
-        feed_category.path_ids.each_with_index.map do |id, idx|
-          @current["#{CATEGORY_ID_KEY}_#{idx}"] = id
-        end
-      else
-        Rails.logger.warn("Offer categoryId is '#{ext_category_id}' but it was not found in <categories>")
-        @current[CATEGORY_ID_KEY] = WRONG_CATEGORY_ERROR
-        @current[CATEGORY_IDS_KEY] = [WRONG_CATEGORY_ERROR]
-        @current["#{CATEGORY_ID_KEY}_0"] = WRONG_CATEGORY_ERROR
-      end
-      # @current[:feed_category_id] = @categories_cache.find_by_ext_id(@current['categoryId'].first[HASH_BABG])
-      # @current[:feed_category_path] = @categories.tree(@current[:feed_category_id])
-    end
-
-    def enrich_indexed_at
-      @current[INDEXED_AT] = Time.current
-    end
-
-    def enrich_favorite_ids
-      @current[FAVORITE_IDS_KEY] = [
-        "advertiser_id:#{context.feed.advertiser.id}",
-        "feed_id:#{context.feed.id}",
-        *@current[CATEGORY_IDS_KEY].map { |feed_category_id| "feed_category_id:#{feed_category_id}" }
-      ]
-    end
-
-    # .reject { |el| el.blank? }
-    # .map { |arr| arr.map { |b| b[HASH_BANG_KEY] }}.join(' - ')
-
-    # class CategoriesCache
-    #   def initialize(feed_id)
-    #     @serialized_tree = FeedCategory.where(feed_id: feed_id)
-    #                            .select('id, ext_id, ancestry_depth as level, ancestry').arrange_serializable
-    #   end
-    #
-    #   def find_by_ext_id(ext_id)
-    #     @serialized_tree.each do |category|
-    #       res = traverse(ext_id, category)
-    #       return res if res
-    #     end
-    #   end
-    #
-    #   def tree
-    #     debugger
-    #     p 1
-    #   end
-    #
-    #   def traverse(ext_id, category)
-    #     if category['ext_id'] == ext_id
-    #       return category['id']
-    #     elsif category['children']
-    #       category['children'].each do |child|
-    #         res = inner_traverse(ext_id, category['children'])
-    #         break res if res
-    #       end
-    #     end
-    #     children.each do |child|
-    #       if category['ext_id'] == ext_id
-    #         break category['id']
-    #       elsif
-    #
-    #       end
-    #     end
-    #
-    #
-    #   end
-    # end
   end
 end
--- spec/interactors/feeds/offers_spec.rb
+++ spec/interactors/feeds/offers_spec.rb
@@ -4,19 +4,15 @@ require 'rails_helper'
 
 describe Feeds::Offers do
   subject do
-    instance = described_class.new(OpenStruct.new(feed: feed))
-    instance.append(doc)
+    described_class.new(OpenStruct.new(feed: feed))
   end
 
-  let(:doc) do
-    Nokogiri::XML(xml).children.first
-  end
-
+  let(:doc) { Nokogiri::XML(xml).children.first }
   let(:advertiser) { create(:advertiser) }
   let(:feed) { create(:feed, advertiser: advertiser) }
+  let!(:feed_category) { create(:feed_category, feed: feed, ext_id: 'category 1') }
 
   describe 'minimal xml offer structure' do
-
     let(:xml) do
       %(
       <offer id="12346">
@@ -26,27 +22,99 @@ describe Feeds::Offers do
       )
     end
 
-    let!(:feed_category) { create(:feed_category, feed: feed, ext_id: 'category 1') }
+    describe '#append' do
+      it 'calls Import::Offers::Hashify with correct argument' do
+        expect(Import::Offers::Hashify).to receive(:call).with({}, doc)
+        subject.append(doc)
+      end
 
-    it 'calls Import::Offers::Language with correct argument' do
-      expect(Import::Offers::Language).to receive(:call).with(
-        include('description' => [include('#' => include('Отличный подарок для любителей венских вафель.'))])
-      )
-      subject
+      it 'calls Import::Offers::Category with correct argument' do
+        expect(Import::Offers::Category).to receive(:call).with(
+          include('categoryId' => [include('#' => 'category 1')]), feed, feed.feed_categories_for_import
+        )
+        subject.append(doc)
+      end
+
+      it 'calls Import::Offers::StandardAttributes with correct argument' do
+        expect(Import::Offers::StandardAttributes).to receive(:call).with(instance_of(Hash), feed)
+        subject.append(doc)
+      end
+
+      it 'calls Import::Offers::Language with correct argument' do
+        expect(Import::Offers::Language).to receive(:call).with(
+          include('description' => [include('#' => include('Отличный подарок для любителей венских вафель.'))])
+        )
+        subject.append(doc)
+      end
+
+      it 'calls Import::Offers::FavoriteIds with correct arguments' do
+        expect(Import::Offers::FavoriteIds).to receive(:call).with(
+          include('feed_category_ids' => [feed_category.id]), advertiser, feed
+        )
+        subject.append(doc)
+      end
+
+      it 'increases total_count' do
+        subject.append(doc)
+        expect(subject.total_count).to eq(1)
+      end
+
+      it 'increases batch_count' do
+        subject.append(doc)
+        expect(subject.batch_count).to eq(1)
+      end
+
+      it 'returns array of offers' do
+        freeze_time do
+          expect(subject.append(doc)).to eq(
+            [
+              '*' => { 'id' => '12346' },
+              'attempt_uuid' => nil,
+              'categoryId' => [
+                { '#' => feed_category.ext_id }
+              ],
+              'description' => [
+                { '#' => 'Отличный подарок для любителей венских вафель.' }
+              ],
+              'detected_language' => {
+                'code' => 'ru',
+                'name' => 'RUSSIAN',
+                'reliable' => true
+              },
+              'favorite_ids' => %w[advertiser_id:1 feed_id:1 feed_category_id:1],
+              'feed_category_id' => feed_category.id,
+              'feed_category_id_0' => feed_category.id,
+              'feed_category_ids' => [feed_category.id],
+              'indexed_at' => Time.current,
+              'name' => []
+            ]
+          )
+        end
+      end
     end
 
-    it 'calls Import::Offers::Category with correct argument' do
-      expect(Import::Offers::Category).to receive(:call).with(
-        include('categoryId' => [include('#' => 'category 1')]), feed, feed.feed_categories_for_import
-      )
-      subject
-    end
+    describe '#flush' do
 
-    it 'calls Import::Offers::FavoriteIds with correct arguments' do
-      expect(Import::Offers::FavoriteIds).to receive(:call).with(
-        include('feed_category_ids' => [feed_category.id]), advertiser, feed
-      )
-      subject
+      it 'calls Import::Offers::Flush with correct arguments' do
+        offers = subject.append(doc)
+        expect(Import::Offers::Flush).to receive(:call).with(
+          offers, advertiser, feed, instance_of(Elasticsearch::Transport::Client)
+        )
+        subject.flush
+      end
+
+      it 'zerofy batch_count' do
+        subject.append(doc)
+        expect(subject.batch_count).to eq(1)
+        subject.flush
+        expect(subject.batch_count).to eq(0)
+      end
+
+      it 'does not zerofy total_count' do
+        subject.append(doc)
+        subject.flush
+        expect(subject.total_count).to eq(1)
+      end
     end
   end
 end
