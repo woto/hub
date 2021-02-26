20be08bd95290fa1dac818fd9adc7a71681052cd
Ruslan <oganer@gmail.com>
2021-02-26 08:58:14 +0300

    style: small renames in favorite_ids_spec.rb

--- spec/interactors/import/offers/favorite_ids_spec.rb
+++ spec/interactors/import/offers/favorite_ids_spec.rb
@@ -3,18 +3,18 @@
 require 'rails_helper'
 
 describe Import::Offers::FavoriteIds do
-  subject { described_class.call(hash, advertiser, feed) }
+  subject { described_class.call(offer, advertiser, feed) }
 
   let(:advertiser) { create(:advertiser) }
   let(:feed) { create(:feed) }
 
   context 'when feed_category_ids is not empty' do
-    let(:hash) { { Import::Offers::Category::CATEGORY_IDS_KEY => category_ids } }
+    let(:offer) { { Import::Offers::Category::CATEGORY_IDS_KEY => category_ids } }
     let(:category_ids) { [Faker::Number.number, Faker::Number.number] }
 
     it 'fills favorite_ids correctly' do
       subject
-      expect(hash).to include(
+      expect(offer).to include(
         Import::Offers::FavoriteIds::FAVORITE_IDS_KEY => contain_exactly(
           "advertiser_id:#{advertiser.id}",
           "feed_id:#{feed.id}",
@@ -25,7 +25,7 @@ describe Import::Offers::FavoriteIds do
   end
 
   context 'when feed_category_ids is empty' do
-    let(:hash) { { Import::Offers::Category::CATEGORY_IDS_KEY => [] } }
+    let(:offer) { { Import::Offers::Category::CATEGORY_IDS_KEY => [] } }
 
     it 'does not raise error' do
       subject
