6d1abbdc5828a5757c84143599a26d836fb7be05
Ruslan <oganer@gmail.com>
2021-02-26 20:41:22 +0300

    style: update Import::Offers::Language

--- app/interactors/import/offers/language.rb
+++ app/interactors/import/offers/language.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 module Import
   module Offers
     class Language
-      LANGUAGE_KEY = "detected_language"
+      LANGUAGE_KEY = 'detected_language'
 
       def self.call(offer)
-        string = offer.values_at('name', 'description').flatten.compact
-                      .map { |el| el[Feeds::Offers::HASH_BANG_KEY] }.join(' - ')
+        proc = ->(el) { el[Import::Offers::Hashify::HASH_BANG_KEY] }
+        string = offer.values_at('name', 'description').flatten.compact.map(&proc).join(' - ')
         offer[LANGUAGE_KEY] = CLD.detect_language(string).stringify_keys
       end
     end
--- spec/interactors/import/offers/language_spec.rb
+++ spec/interactors/import/offers/language_spec.rb
@@ -3,34 +3,34 @@
 require 'rails_helper'
 
 describe Import::Offers::Language do
-  subject { described_class.call(params) }
+  subject { described_class.call(offer) }
 
   context 'when description includes russian language' do
-    let(:params) { { 'description' => [Feeds::Offers::HASH_BANG_KEY => 'Это длинный текст описания на русском.'] } }
+    let(:offer) { { 'description' => [Import::Offers::Hashify::HASH_BANG_KEY => 'Это длинный текст описания на русском.'] } }
 
-    it 'modifies params with detected language' do
+    it 'modifies offer with detected language' do
       subject
-      expect(params).to include(Import::Offers::Language::LANGUAGE_KEY =>
+      expect(offer).to include(Import::Offers::Language::LANGUAGE_KEY =>
                                   { 'name' => 'RUSSIAN', 'code' => 'ru', 'reliable' => true })
     end
   end
 
   context 'when description includes english language' do
-    let(:params) { { 'description' => [Feeds::Offers::HASH_BANG_KEY => 'Phrase and clause cover everything.'] } }
+    let(:offer) { { 'description' => [Import::Offers::Hashify::HASH_BANG_KEY => 'Phrase and clause cover everything.'] } }
 
-    it 'modifies params with detected language' do
+    it 'modifies offer with detected language' do
       subject
-      expect(params).to include(Import::Offers::Language::LANGUAGE_KEY =>
+      expect(offer).to include(Import::Offers::Language::LANGUAGE_KEY =>
                                   { 'name' => 'ENGLISH', 'code' => 'en', 'reliable' => true })
     end
   end
 
   context 'when name and description are empty' do
-    let(:params) { { 'description' => [Feeds::Offers::HASH_BANG_KEY => ''] } }
+    let(:offer) { { 'description' => [Import::Offers::Hashify::HASH_BANG_KEY => ''] } }
 
-    it 'detects unknown langugage and stores results in params' do
+    it 'detects unknown langugage and stores results in offer' do
       subject
-      expect(params).to include(Import::Offers::Language::LANGUAGE_KEY =>
+      expect(offer).to include(Import::Offers::Language::LANGUAGE_KEY =>
                                   { 'name' => 'Unknown', 'code' => 'un', 'reliable' => true })
     end
   end
