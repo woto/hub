4c2c038dee05cc10e42837e9d398fef23abf76ed
Ruslan <oganer@gmail.com>
2021-02-26 20:27:38 +0300

    feat:  add Import::Offers::StandardAttributes

---
+++ app/interactors/import/offers/standard_attributes.rb
@@ -0,0 +1,13 @@
+module Import
+  module Offers
+    class StandardAttributes
+      ATTEMPT_UUID_KEY = 'attempt_uuid'
+      INDEXED_AT_KEY = 'indexed_at'
+
+      def self.call(offer, feed)
+        offer[ATTEMPT_UUID_KEY] = feed.attempt_uuid
+        offer[INDEXED_AT_KEY] = Time.current
+      end
+    end
+  end
+end
---
+++ spec/interactors/import/offers/standard_attributes_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Import::Offers::StandardAttributes do
+  subject { described_class.call(offer, feed) }
+
+  let(:feed) { create(:feed, :with_attempt_uuid) }
+  let(:offer) { {} }
+
+  it 'sets feed attempt_uuid to offer hash' do
+    freeze_time do
+      subject
+      expect(offer).to include('attempt_uuid' => feed.attempt_uuid,
+                               'indexed_at' => Time.current)
+    end
+  end
+end
