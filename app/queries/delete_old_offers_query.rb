8af5eb3bbdfaa37fb7c8c37634c9fa614531a9fc
Ruslan <oganer@gmail.com>
2021-02-28 02:45:09 +0300

    feat: add DeleteOldOffersQuery

---
+++ app/queries/delete_old_offers_query.rb
@@ -0,0 +1,40 @@
+class DeleteOldOffersQuery
+  include ApplicationInteractor
+
+  class Contract < Dry::Validation::Contract
+    params do
+      config.validate_keys = true
+      required(:feed).value(type?: Feed)
+    end
+  end
+
+  before do
+    result = Contract.new.call(context.to_h)
+    raise result.inspect if result.failure?
+  end
+
+  def call
+    body = Jbuilder.encode do |json|
+      json.query do
+        json.bool do
+          json.must_not do
+            json.term do
+              json.set! 'attempt_uuid.keyword', context.feed.attempt_uuid
+            end
+          end
+          json.filter do
+            json.term do
+              json.feed_id context.feed.id
+            end
+          end
+        end
+      end
+    end
+
+    context.object = {
+      index: Elastic::IndexName.offers,
+      routing: context.feed.id,
+      body: body
+    }
+  end
+end
---
+++ spec/queries/delete_old_offers_query_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe DeleteOldOffersQuery do
+  subject { described_class.call(feed: feed) }
+
+  let(:feed) { create(:feed, :with_attempt_uuid) }
+
+  it 'builds correct query' do
+    expect(subject.object).to eq(
+      index: Elastic::IndexName.offers,
+      routing: feed.id,
+      body: {
+        query: {
+          bool: {
+            must_not: {
+              term: {
+                'attempt_uuid.keyword' => feed.attempt_uuid
+              }
+            },
+            filter: {
+              term: {
+                'feed_id' => feed.id
+              }
+            }
+          }
+        }
+      }.to_json
+    )
+  end
+end
