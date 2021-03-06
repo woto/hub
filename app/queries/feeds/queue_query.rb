1fdffbba1d0ab39861131ce5f51053a2e680dcfd
Ruslan <oganer@gmail.com>
2021-03-06 09:17:46 +0300

    refactor: remove old Feeds::QueueQuery

--- app/queries/feeds/queue_query.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Feeds::QueueQuery
-  def self.call
-    Feed
-      .where(locked_by_pid: 0)
-      .order('priority DESC, processing_finished_at ASC NULLS FIRST')
-  end
-end
