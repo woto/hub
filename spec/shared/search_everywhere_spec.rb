db0fd9dac3cc8f27784010b8c79b0d7b919ee116
Ruslan <oganer@gmail.com>
2021-03-07 05:26:23 +0300

    feat: commit shared_search_everywhere

---
+++ spec/shared/search_everywhere_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+shared_examples 'shared_search_everywhere' do
+  let(:q) { Faker::Lorem.word }
+
+  it 'clicking on "search everywhere" leads to the correct address' do
+    within('section.page') do
+      fill_in 'Введите текст для поиска...', with: q
+      click_button 'search-button'
+      expect(page).to have_current_path(
+        url_for(params),
+        url: false
+      )
+    end
+  end
+end
