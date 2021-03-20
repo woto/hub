4801da8c5936f41281dfce20e5ffac72c72da4c3
Ruslan <oganer@gmail.com>
2021-03-20 08:21:50 +0300

    feat(Offers::Group): add test for nils

--- app/queries/offers/group.rb
+++ app/queries/offers/group.rb
@@ -9,8 +9,8 @@ module Offers
       params do
         config.validate_keys = true
         required(:json)
-        optional(:group_by).filled(:string)
-        optional(:include).maybe { array? { each { string? } } }
+        required(:group_by).maybe(:string)
+        required(:include).maybe { array? { each { string? } } }
       end
     end
 
@@ -26,26 +26,28 @@ module Offers
     private
 
     def _group
-      json.aggs do
-        json.set! GlobalHelper::GROUP_NAME do
-          json.terms do
-            json.field context.group_by
-            json.order do
-              json.set! 'sort.sum_of_squares', 'desc'
-            end
-            json.size GlobalHelper::GROUP_LIMIT
-            json.include context.include if context.include
-          end
-          json.aggs do
-            json.offers do
-              json.top_hits do
-                json.size GlobalHelper::MULTIPLICATOR - 1
+      if context.group_by.present?
+        json.aggs do
+          json.set! GlobalHelper::GROUP_NAME do
+            json.terms do
+              json.field context.group_by
+              json.order do
+                json.set! 'sort.sum_of_squares', 'desc'
               end
+              json.size GlobalHelper::GROUP_LIMIT
+              json.include context.include if context.include
             end
-            json.sort do
-              json.extended_stats do
-                json.script do
-                  json.source '_score'
+            json.aggs do
+              json.offers do
+                json.top_hits do
+                  json.size GlobalHelper::MULTIPLICATOR - 1
+                end
+              end
+              json.sort do
+                json.extended_stats do
+                  json.script do
+                    json.source '_score'
+                  end
                 end
               end
             end
--- spec/queries/offers/group_spec.rb
+++ spec/queries/offers/group_spec.rb
@@ -3,7 +3,32 @@
 require 'rails_helper'
 
 describe Offers::Group do
-  context 'when `include` param is present' do
+
+  context 'when `group_by` param is nil' do
+    subject do
+      Jbuilder.new do |json|
+        described_class.call(json: json, group_by: nil, include: nil)
+      end.attributes!.deep_symbolize_keys
+    end
+
+    it 'returns correct json' do
+      expect(subject).to eq({})
+    end
+  end
+
+  context 'when `include` param is nil' do
+    subject do
+      Jbuilder.new do |json|
+        described_class.call(json: json, group_by: 'group_by', include: nil)
+      end.attributes!.deep_symbolize_keys
+    end
+
+    it 'does not `include` include param' do
+      expect(subject[:aggs][GlobalHelper::GROUP_NAME][:terms]).not_to have_key(:include)
+    end
+  end
+
+  context 'when `group_by` and `include` params are present' do
     subject do
       Jbuilder.new do |json|
         described_class.call(json: json, group_by: 'group_by', include: ['include'])
@@ -19,42 +44,6 @@ describe Offers::Group do
               order: { "sort.sum_of_squares": 'desc' },
               size: GlobalHelper::GROUP_LIMIT,
               include: ['include']
-            },
-            aggs: {
-              offers: {
-                top_hits: {
-                  size: GlobalHelper::MULTIPLICATOR - 1
-                }
-              },
-              sort: {
-                extended_stats: {
-                  script: {
-                    source: '_score'
-                  }
-                }
-              }
-            }
-          }
-        }
-      )
-    end
-  end
-
-  context 'when `include` param is not present' do
-    subject do
-      Jbuilder.new do |json|
-        described_class.call(json: json, group_by: 'group_by')
-      end.attributes!.deep_symbolize_keys
-    end
-
-    it 'returns correct json' do
-      expect(subject).to eq(
-        aggs: {
-          GlobalHelper::GROUP_NAME => {
-            terms: {
-              field: 'group_by',
-              order: { "sort.sum_of_squares": 'desc' },
-              size: GlobalHelper::GROUP_LIMIT
             },
             aggs: {
               offers: {
