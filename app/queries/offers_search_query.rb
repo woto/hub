47e5c9f48e57f6f50f5e7aa89862b1663251cdce
Ruslan <oganer@gmail.com>
2021-03-20 08:29:27 +0300

    feat: commit Offers::SearchQuery

--- app/queries/offers_search_query.rb
+++ app/queries/offers_search_query.rb
@@ -2,129 +2,84 @@
 
 class OffersSearchQuery
   include ApplicationInteractor
-  include Elasticsearch::DSL
 
-  # Code fully covered by tests in spec/lib/queries/offers_search_query_spec.rb
+  class Contract < Dry::Validation::Contract
+    params do
+      config.validate_keys = true
+      required(:q).maybe(:string)
+      required(:from).filled(:integer)
+      required(:size).filled(:integer)
+      required(:group_by).maybe(:string)
+      required(:filter_id).maybe { array? { each { string? } } }
+      required(:include).maybe { array? { each { string? } } }
+      required(:filter_by).maybe(:string)
+    end
+  end
+
+  before do
+    result = Contract.new.call(context.to_h)
+    raise result.inspect if result.failure?
+  end
+
   def call
-    search_tokens = Elastic::Tokenize.call(q: context.q).object
-    search_string = search_tokens.join(' ')
-
-    body = Jbuilder.encode do |json|
-      json.aggs do
-        json.vendors do
-          json.terms do
-            json.field 'vendor.#.keyword'
-            json.size 30
-          end
-        end
-      end
-
-      # TODO prices
-      #
-      # json.aggs do
-      #   json.max_price do
-      #     json.max do
-      #       json.field 'price.#.keyword'
-      #     end
-      #   end
-      # end
-      #
-      # json.aggs do
-      #   json.min_price do
-      #     json.min do
-      #       json.field 'price.#.keyword'
-      #     end
-      #   end
-      # end
-
-      json.aggs do
-        json.group do
-          json.terms do
-            json.field context.group_by
-            json.order do
-              json.set! 'sort.sum_of_squares', 'desc'
-            end
-            json.size 1000
-          end
-          json.aggs do
-            json.offers do
-              json.top_hits do
-                json.size 6
-              end
-            end
-            json.sort do
-              json.extended_stats do
-                json.script do
-                  json.source '_score'
-                end
-              end
-            end
-          end
-        end
-      end
-
+    body = Jbuilder.new do |json|
       json.query do
         json.bool do
+          Offers::Filters.call(
+            json: json,
+            filter_by: context.filter_by,
+            filter_id: context.filter_id
+          ).object
 
-          if context.advertiser_id.present?
-            json.set! :filter do
-              json.array! ['fuck'] do
-                json.term do
-                  json.advertiser_id context.advertiser_id
-                end
-              end
-            end
-          elsif context.feed_id.present?
-            json.set! :filter do
-              json.array! ['fuck'] do
-                json.term do
-                  json.feed_id context.feed_id
-                end
-              end
-            end
-          elsif context.feed_category_id
-            json.set! :filter do
-              json.array! ['fuck'] do
-                json.term do
-                  # TODO
-                  # if context.only
-                  #   term Feeds::Offers::CATEGORY_ID_KEY => context.category_id
-                  json.set! "#{Feeds::Offers::CATEGORY_ID_KEY}_#{context.current_category_level}", context.feed_category_id
-                end
-              end
-            end
-          end
-
-          json.set! :should do
-            json.array! ['fuck'] do
-              json.multi_match do
-                json.query search_string
-                json.fields %w[name.#^3 feed_category_name.# description.#]
-              end
-            end
-          end
-
-          json.set! :filter do
-            json.array! ['fuck'] do
-              json.multi_match do
-                json.query search_string
-                json.fields %w[name.# feed_category_name.# description.#]
-                json.fuzziness 'auto'
-                json.minimum_should_match search_tokens.count
-              end
-            end
-          end
-
+          Offers::SearchString.call(
+            json: json,
+            q: context.q
+          ).object
         end
       end
-    end
+
+      Offers::Group.call(
+        json: json,
+        group_by: context.group_by,
+        include: context.include
+      ).object
+    end.attributes!.deep_symbolize_keys
 
     context.object = {}.tap do |h|
       h[:body] = body
       h[:index] = ::Elastic::IndexName.offers
+      h[:size] = context.size
       h[:from] = context.from
-      h[:size] = 0
+      # TODO: Change to advertiser? It does not work for now.
       h[:routing] = context.feed_id if context.feed_id.present?
     end
   end
 end
+
+# TODO: vendors aggregation
+# json.aggs do
+#   json.vendors do
+#     json.terms do
+#       json.field 'vendor.#.keyword'
+#       json.size 30
+#     end
+#   end
+# end
+
+# TODO: prices
+# json.aggs do
+#   json.max_price do
+#     json.max do
+#       json.field 'price.#.keyword'
+#     end
+#   end
+# end
+
+# TODO: prices
+# json.aggs do
+#   json.min_price do
+#     json.min do
+#       json.field 'price.#.keyword'
+#     end
+#   end
+# end
--- spec/lib/queries/offers_search_query_spec.rb
+++ spec/lib/queries/offers_search_query_spec.rb
@@ -2,223 +2,47 @@
 
 require 'rails_helper'
 
+# rubocop: disable Metrics/BlockLength
 describe OffersSearchQuery do
   subject { described_class.call(args).object }
 
-  context 'with context.feed_id' do
-    context 'when context.feed_id was passed' do
-      let(:args) { { feed_id: '1-offer_id+2-feed_id' } }
+  let(:from) { 1 }
+  let(:size) { 10 }
 
-      specify do
-        expect(subject).to include(index: 'test.offers')
-      end
-
-      specify do
-        expect(subject).to include(routing: 2)
-      end
-
-      specify do
-        expect(subject).to include(
-          body: include(
-            query: {
-              bool: {
-                filter: include(
-                  term: { "feed_id": 2 }
-                )
-              }
-            }
-          )
-        )
-      end
-    end
-
-    context 'when context.feed_id was not passed' do
-      let(:args) { {} }
-
-      specify do
-        expect(subject).to include(index: 'test.offers')
-      end
-
-      specify do
-        expect(subject).not_to include(:routing)
-      end
-
-      specify do
-        expect(subject).to include(
-          body: include(
-            aggregations: include(
-              feeds: {
-                terms: {
-                  field: 'feed_id',
-                  size: 20
-                }
-              }
-            )
-          )
-        )
-      end
-    end
+  let(:args) do
+    {
+      q: 'q',
+      from: from,
+      size: size,
+      group_by: 'group_by',
+      filter_by: 'filter_by',
+      filter_id: ['filter_id'],
+      include: ['include']
+    }
   end
 
-  context 'with context.q' do
-    context 'when context.q passed' do
-      let(:args) { { q: 'Some text' } }
+  specify do
+    expect(Offers::Filters).to(
+      receive(:call).with(json: anything, filter_by: 'filter_by', filter_id: ['filter_id']).and_call_original
+    )
 
-      specify do
-        expect(subject).to include(
-          body: include(
-            {
-              query: {
-                bool: {
-                  must: [{
-                    query_string: { query: 'Some text' }
-                  }]
-                }
-              }
-            },
-            {
-              highlight: {
-                fields: { "description.#": {}, "name.#": {} },
-                tags_schema: :styled
-              }
-            }
-          )
-        )
-      end
-    end
+    expect(Offers::SearchString).to(
+      receive(:call).with(json: anything, q: 'q').and_call_original
+    )
 
-    context 'when context.q was not passed' do
-      let(:args) { {} }
+    expect(Offers::Group).to(
+      receive(:call).with(json: anything, group_by: 'group_by', include: ['include']).and_call_original
+    )
 
-      specify do
-        expect(subject).to include(
-          body: include(
-            query: {
-              bool: {
-                filter: [{ match_all: {} }]
-              }
-            }
-          )
-        )
-      end
-    end
-  end
-
-  # could not find `profile` in elasticsearch-dsl
-  xcontext 'when context.profile passed' do
-    let(:args) { { profile: '1' } }
-
-    specify do
-      expect(subject).to include(body: include(profile: true))
-    end
-  end
-
-  pending 'per'
-  pending 'page'
-  pending 'size'
-
-  context 'when context.feed_id and context.category_id and context.q are blank' do
-    let(:args) {}
-
-    specify do
-      expect(subject).to include(
-        body: include(
-          sort: [
-            { indexed_at: { order: :desc } }
-          ]
-        )
-      )
-    end
-  end
-
-  context 'with context.category_id' do
-    let(:feed) { create(:feed) }
-
-    context 'when context.category_id was not passed' do
-      let(:args) { { feed_id: feed.slug_with_advertiser } }
-
-      specify do
-        expect(subject).to include(
-          body: include(
-            aggregations: include(
-              categories: {
-                terms: {
-                  field: 'category_level_0',
-                  size: 20
-                }
-              }
-            )
-          )
-        )
-      end
-    end
-
-    context 'when context.category_id was passed' do
-      let(:parent) { create(:feed_category, feed: feed) }
-      let(:category) { create(:feed_category, parent: parent, feed: feed) }
-      let(:level) { category.ancestry_depth }
-
-      context 'regardless of context.only' do
-        let(:args) { { feed_id: feed.slug_with_advertiser, category_id: category.id } }
-
-        specify do
-          expect(subject).to include(
-            body: include(
-              aggregations: include(
-                categories: {
-                  terms: {
-                    field: "category_level_#{level + 1}",
-                    size: 20
-                  }
-                },
-                category: {
-                  filter: {
-                    term: {
-                      feed_category_id: category.id
-                    }
-                  }
-                }
-              )
-            )
-          )
-        end
-      end
-
-      context 'without context.only' do
-        let(:args) { { category_id: category.id } }
-
-        specify do
-          expect(subject).to include(
-            body: include(
-              query: {
-                bool: {
-                  filter: include(
-                    term: { "category_level_#{level}": category.id }
-                  )
-                }
-              }
-            )
-          )
-        end
-      end
-
-      context 'with context.only' do
-        let(:args) { { category_id: category.id, only: '1' } }
-
-        specify do
-          expect(subject).to include(
-            body: include(
-              query: {
-                bool: {
-                  filter: include(
-                    term: { 'feed_category_id': category.id }
-                  )
-                }
-              }
-            )
-          )
-        end
-      end
-    end
+    expect(subject).to match(
+      body: match(
+        aggs: include({}),
+        query: { bool: include(:filter, :should) }
+      ),
+      from: from,
+      index: Elastic::IndexName.offers,
+      size: size
+    )
   end
 end
+# rubocop: enable Metrics/BlockLength
