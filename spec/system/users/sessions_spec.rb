0b9b15574b0df222cee94128d2890280a7f43351
Ruslan <oganer@gmail.com>
2021-04-10 15:01:10 +0300

    feat: commit devise login

--- spec/system/users/sessions_spec.rb
+++ spec/system/users/sessions_spec.rb
@@ -6,35 +6,35 @@ describe Users::SessionsController do
   let(:user) { create(:user) }
 
   it 'signs in successfully' do
-    visit '/auth/login'
+    visit '/ru/auth/login'
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
     click_button 'login'
     expect_dashboard
     expect_authenticated
-    expect(page).to have_text('Signed in successfully.')
+    expect(page).to have_text('Вход в систему выполнен.')
   end
 
   it 'displays invalid email or password' do
-    visit '/auth/login'
+    visit '/ru/auth/login'
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: Faker::Alphanumeric.alphanumeric
     click_button 'login'
-    expect(page).to have_text('Invalid Email or password.')
+    expect(page).to have_text('Неправильный Email или пароль.')
   end
 
   it 'signes out successfully' do
     login_as(user, scope: :user)
-    visit '/dashboard'
+    visit '/ru/dashboard'
     find('.capybara-desktop.capybara-authenticated').click
     click_link 'logout'
-    expect(page).to have_text('Signed out successfully.')
+    expect(page).to have_text('Выход из системы выполнен.')
   end
 
   it 'displays you are already signed in' do
     login_as(user, scope: :user)
-    visit '/auth/login'
-    expect(page).to have_text('You are already signed in.')
+    visit '/ru/auth/login'
+    expect(page).to have_text('Вы уже вошли в систему.')
   end
 
   it 'tests warden #login_as helper' do
