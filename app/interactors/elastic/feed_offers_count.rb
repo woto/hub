9ee84681cc236e61187f82cde6a500be2566f2e3
Ruslan <oganer@gmail.com>
2021-05-13 13:12:57 +0300

    refactor: contract Elastic::FeedOffersCount

--- app/interactors/elastic/feed_offers_count.rb
+++ app/interactors/elastic/feed_offers_count.rb
@@ -4,17 +4,14 @@ module Elastic
   class FeedOffersCount
     include ApplicationInteractor
 
-    class Contract < Dry::Validation::Contract
+    contract do
       params do
         config.validate_keys = true
         required(:feed).maybe(type?: Feed)
       end
     end
 
-    before do
-      result = Contract.new.call(context.to_h)
-      raise result.inspect if result.failure?
-    end
+
 
     def call
       query = FeedOffersCountQuery.call(feed: context.feed).object
