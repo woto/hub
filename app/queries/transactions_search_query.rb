3fd78aeb3dcc3c0f7dfb529218f3707e3b32343d
Ruslan <oganer@gmail.com>
2021-05-13 14:14:02 +0300

    feat: commit TransactionsSearchQuery

--- app/queries/transactions_search_query.rb
+++ app/queries/transactions_search_query.rb
@@ -2,44 +2,70 @@
 
 class TransactionsSearchQuery
   include ApplicationInteractor
-  include Elasticsearch::DSL
+
+  contract do
+    params do
+      config.validate_keys = true
+      required(:q).maybe(:string)
+      required(:from).filled(:integer)
+      required(:size).filled(:integer)
+      required(:filter_ids).maybe { array? { each { string? } } }
+      required(:sort).maybe(:string)
+      required(:order).maybe(:string)
+      required(:locale).maybe(:symbol)
+    end
+  end
 
   def call
-    definition = search do
-
-      query do
-        bool do
-          if context.filter_ids
-            filter do
-              terms "debit_id" => context.filter_ids
-            end
-          end
-
-          if context.q.present?
-            must do
-              query_string do
-                query context.q
+    body = Jbuilder.new do |json|
+      json.query do
+        if context.filter_ids
+          json.bool do
+            json.must do
+              json.array! ['fuck!'] do
+                json.bool do
+                  json.set! :should do
+                    json.array! ['fuck'] do
+                      json.terms do
+                        json.credit_id context.filter_ids
+                      end
+                    end
+                    json.array! ['fuck'] do
+                      json.terms do
+                        json.debit_id context.filter_ids
+                      end
+                    end
+                  end
+                end
               end
-            end
-          else
-            filter do
-              match_all {}
             end
           end
         end
       end
 
-      sort do
-        by context.sort, order: context.order
+      json.sort do
+        json.set! context.sort do
+          json.order context.order
+        end
       end
-    end
+      # sort do
+      #
+      # end
+    end.attributes!.deep_symbolize_keys
 
     context.object = {}.tap do |h|
-      h[:body] = definition.to_hash.deep_symbolize_keys
+      h[:body] = body
       h[:index] = ::Elastic::IndexName.transactions
-      h[:from] = context.from
       h[:size] = context.size
-      h[:_source] = context._source
+      h[:from] = context.from
     end
+
+    # context.object = {}.tap do |h|
+    #   h[:body] = definition.to_hash.deep_symbolize_keys
+    #   h[:index] = ::Elastic::IndexName.transactions
+    #   h[:from] = context.from
+    #   h[:size] = context.size
+    #   h[:_source] = context._source
+    # end
   end
 end
