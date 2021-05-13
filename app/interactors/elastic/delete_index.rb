62dc562486d2738f9bf1938ceada2526e8681cbc
Ruslan <oganer@gmail.com>
2021-05-13 13:11:55 +0300

    refactor: contract Elastic::DeleteIndex

--- app/interactors/elastic/delete_index.rb
+++ app/interactors/elastic/delete_index.rb
@@ -4,17 +4,14 @@ module Elastic
   class DeleteIndex
     include ApplicationInteractor
 
-    class Contract < Dry::Validation::Contract
+    contract do
       params do
         required(:index_name).filled(:string)
         optional(:ignore_unavailable).maybe(:bool)
       end
     end
 
-    before do
-      result = Contract.new.call(context.to_h)
-      raise result.inspect if result.failure?
-    end
+
 
     def call
       Rails.logger.info(message: 'Deleting elasticsearch index', index: context.index_name)
