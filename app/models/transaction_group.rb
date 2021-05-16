67a64243b986b59a054d2bcf6a74f50b89a64521
Ruslan <oganer@gmail.com>
2021-05-16 20:51:53 +0300

    feat: commit TransactionGroup with specs

--- app/models/transaction_group.rb
+++ app/models/transaction_group.rb
@@ -11,21 +11,12 @@
 #
 class TransactionGroup < ApplicationRecord
   enum kind: {
-    pending_post: 0,
-    accrued_post: 2,
-    pending_to_accrued: 3,
-    accrued_to_canceled: 4,
-    accrued_to_rejected: 5,
-    accrued_to_pending: 6,
-    accrued_to_paid_with_yandex: 7,
-    advertiser_to_hub_payed_rub: 8,
-    stakeholder_to_hub: 9,
-    hub_rub_to_advego_account_usd: 10,
-    request_check: 11,
-    pay_check: 12,
-    create_or_change_pending: 13,
-    rejected_to_pending: 14
+    'accounting/main/change_status': 0
   }
 
   has_many :transactions
+
+  def self.start(kind, &block)
+    self.create!(kind: kind.name.underscore).tap(&block)
+  end
 end
--- spec/models/transaction_group_spec.rb
+++ spec/models/transaction_group_spec.rb
@@ -13,5 +13,17 @@ require 'rails_helper'
 
 describe TransactionGroup, type: :model do
   # it_behaves_like 'elasticable'
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to define_enum_for(:kind).with_values(['accounting/main/change_status']) }
+  it { is_expected.to have_many(:transactions) }
+
+  describe '#start' do
+    let(:block) { ->(_) {} }
+
+    specify do
+      transaction_group = instance_double(TransactionGroup)
+      expect(described_class).to receive(:create!).with(kind: 'accounting/main/change_status').and_return(transaction_group)
+      expect(transaction_group).to receive(:tap).and_return(transaction_group)
+      described_class.start(Accounting::Main::ChangeStatus, &block)
+    end
+  end
 end
