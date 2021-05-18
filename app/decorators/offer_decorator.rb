f5582a1f567536b11d8217e50574ef660b234a82
Ruslan <oganer@gmail.com>
2021-05-19 00:10:59 +0300

    feat: commit offer carousel

--- app/decorators/offer_decorator.rb
+++ app/decorators/offer_decorator.rb
@@ -1,10 +1,24 @@
+# frozen_string_literal: true
+
 class OfferDecorator < ApplicationDecorator
   def pictures
-    object['_source']['picture'] || object['_source']['image']
+    (
+      object['_source']['picture'] ||
+        object['_source']['image'] ||
+        # Triple forward slash is not error. By the way it is ironically that if image will not be found
+        # in IMGPROXY_LOCAL_FILESYSTEM_ROOT it will fallback to the same image due to IMGPROXY_FALLBACK_IMAGE_PATH.
+        # You could check local serving behaviour by commenting IMGPROXY_FALLBACK_IMAGE_PATH
+        [{ Import::Offers::Hashify::HASH_BANG_KEY => 'local:///image-not-found.png' }]
+    )&.slice(0, 10)
   end
 
   def name
     names = object.dig('_source', 'name')
-    names && names.map { |item| h.strip_tags(item[Import::Offers::Hashify::HASH_BANG_KEY]) }.join(', ')
+    names = names&.map { |item| h.strip_tags(item[Import::Offers::Hashify::HASH_BANG_KEY]) }&.join(', ')
+    h.truncate(names.to_s, length: 100)
+  end
+
+  def url
+    super.first[Import::Offers::Hashify::HASH_BANG_KEY]
   end
 end
