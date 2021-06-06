e13da289b5cd3d8550f6579c3570827e656435b4
Ruslan <oganer@gmail.com>
2021-06-06 21:44:35 +0300

    feat: add post's user attribute to show post page

--- spec/integration/posts/show_spec.rb
+++ spec/integration/posts/show_spec.rb
@@ -2,13 +2,13 @@
 
 require 'rails_helper'
 
-describe Tables::PostsController, type: :system do
+describe PostsController, type: :system, responsible: :user do
   let(:post) do
     create(:post,
            user: Current.responsible,
-           created_at: 3.minutes.ago,
-           updated_at: 2.minutes.ago,
-           published_at: 1.minute.ago,
+           created_at: 3.hours.ago,
+           # updated_at: 2.hours.ago,
+           published_at: 1.hour.ago,
            tags: ['', 'Some post tag'],
            status: :draft_post,
            currency: :rub,
@@ -21,46 +21,58 @@ describe Tables::PostsController, type: :system do
            })
   end
 
-  it 'shows Post model attributes correctly' do
-    login_as(Current.responsible, scope: :user)
-    visit post_path(post, locale: :ru)
-
-    # meta block
-    expect(page).to have_text('Some post title')
-    expect(page).to have_text(post.realm.to_label)
-    expect(page).to have_text(post.post_category.title)
-    expect(page).to have_text('Some post tag')
-    expect(page).to have_text('3 минуты назад')
-    expect(page).to have_text('несколько секунд назад')
-    expect(page).to have_text('черновик')
-    expect(page).to have_text('₽1,00')
-
-    # extra options block
-    expect(page).to have_field('Копирайт', checked: true, disabled: true)
-    expect(page).to have_field('на сайте advego.ru', checked: true, disabled: true)
-    expect(page).to have_text('Рерайт - пересказ общедоступного текста в интернете')
-
-    # content block
-    expect(page).to have_text('Body. Some long text')
-  end
-
-  context 'when visitor is admin' do
-    it 'shows additional fields' do
-      login_as(create(:user, role: :admin), scope: :user)
+  context 'with user' do
+    it 'shows Post model attributes correctly' do
+      login_as(Current.responsible, scope: :user)
       visit post_path(post, locale: :ru)
 
-      expect(page).to have_text('Intro. Post introduction')
-      # published_at
-      expect(page).to have_text('минуту назад')
+      within('main') do
+        # meta block
+        expect(page).to have_text('Some post title')
+        expect(page).to have_text(post.realm.to_label)
+        expect(page).to have_text(post.post_category.title)
+        expect(page).to have_text('Some post tag')
+        expect(page).to have_text('3 часа назад')
+        expect(page).to have_text('несколько секунд назад')
+        expect(page).to have_text('черновик')
+        expect(page).to have_text('₽1,00')
+
+        # extra options block
+        expect(page).to have_field('Копирайт', checked: true, disabled: true)
+        expect(page).to have_field('на сайте advego.ru', checked: true, disabled: true)
+        expect(page).to have_text('Рерайт - пересказ общедоступного текста в интернете')
+
+        # content block
+        expect(page).to have_text('Body. Some long text')
+
+        # visible to admin
+        expect(page).to have_no_text('Intro. Post introduction')
+        expect(page).to have_no_text(post.user.to_label)
+        expect(page).to have_no_text('час назад')
+      end
     end
   end
 
-  context 'when user does not own the post' do
+  context 'with another user' do
     it 'returns friendly error' do
       login_as(create(:user), scope: :user)
       visit post_path(post, locale: :ru)
 
       expect(page).to have_text("The page you were looking for doesn't exist")
+    end
+  end
+
+
+  context 'with admin' do
+    it 'shows additional fields' do
+      login_as(create(:user, role: :admin), scope: :user)
+      visit post_path(post, locale: :ru)
+
+      within('main') do
+        expect(page).to have_text('Intro. Post introduction')
+        expect(page).to have_text(post.user.to_label)
+        expect(page).to have_text('час назад')
+      end
     end
   end
 end
