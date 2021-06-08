14e90f5035284a6bc80028bbcc8b0ba338a6aebf
Ruslan <oganer@gmail.com>
2021-06-08 21:11:51 +0300

    feat: commit profile model

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -25,6 +25,5 @@
 require 'rails_helper'
 
 describe Profile, type: :model do
-  # it_behaves_like 'elasticable'
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:user).counter_cache(true).touch(true) }
 end
