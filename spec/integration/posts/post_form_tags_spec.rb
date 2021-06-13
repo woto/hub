d7e6472a1acf0aead06c8a50787aef4f92ba1a0d
Ruslan <oganer@gmail.com>
2021-06-13 22:32:20 +0300

    test: add test for adding new tags to post_form_tags_spec.rb

--- spec/integration/posts/post_form_tags_spec.rb
+++ spec/integration/posts/post_form_tags_spec.rb
@@ -24,4 +24,20 @@ describe PostsController, type: :system, responsible: :admin do
 
     expect(page).to have_select('post[tags][]', visible: :hidden, selected: %w[first third])
   end
+
+  it 'allows to add new tags' do
+    login_as(Current.responsible, scope: :user)
+    visit edit_post_path(post, locale: :ru)
+
+    expect(page).to have_select('post[tags][]', visible: :hidden, selected: %w[first second])
+
+    within '.post_tags' do
+      find('.selectize-input').click
+      find('input').native.send_key(:backspace)
+      find('input').native.send_key('fourth')
+      find('input').native.send_key(:enter)
+    end
+
+    expect(page).to have_select('post[tags][]', visible: :hidden, selected: %w[first fourth])
+  end
 end
