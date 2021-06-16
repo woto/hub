25a7b062ec3be4f86967eaa2e65eaf713633c232
Ruslan <oganer@gmail.com>
2021-06-17 00:52:18 +0300

    feat: commit models/favorites_item.rb

--- app/models/favorites_item.rb
+++ app/models/favorites_item.rb
@@ -20,10 +20,56 @@
 #  fk_rails_...  (favorite_id => favorites.id)
 #
 class FavoritesItem < ApplicationRecord
-  belongs_to :favorite, counter_cache: true
+  belongs_to :favorite, counter_cache: true, touch: true
 
-  enum kind: { users: 0, posts: 1, transactions: 2, accounts: 3, checks: 4, news: 5, feeds: 6,
+  enum kind: { users: 0, posts: 1, transactions: 2, accounts: 3, checks: 4, feeds: 5, post_categories: 6,
                advertiser_id: 7, feed_id: 8, feed_category_id: 9, _id: 10 }
 
   validates :kind, presence: true
+  validate :kind, :check_kind_matching
+
+  def self.favorites_item_kind_to_favorite_kind(kind)
+    case kind
+    when 'advertiser_id', 'feed_id', 'feed_category_id', '_id'
+      'offers'
+    else
+      kind
+    end
+  end
+
+  private
+
+  def check_kind_matching
+    return unless kind
+    return unless favorite
+
+    errors.add(:kind, :invalid) if self.class.favorites_item_kind_to_favorite_kind(kind) != favorite.kind
+
+    # case kind
+    # when 'accounts'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'accounts'
+    # when 'feeds'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'feeds'
+    # when 'posts'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'posts'
+    # when 'users'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'users'
+    # when 'transactions'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'transactions'
+    # when 'checks'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'checks'
+    # when 'post_categories'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'post_categories'
+    # when 'advertiser_id'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'offers'
+    # when 'feed_id'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'offers'
+    # when 'feed_category_id'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'offers'
+    # when '_id'
+    #   errors.add(:kind, :invalid) if favorite.kind != 'offers'
+    # else
+    #   raise "kind `#{kind}` is not accounted"
+    # end
+  end
 end
--- spec/models/favorites_item_spec.rb
+++ spec/models/favorites_item_spec.rb
@@ -22,6 +22,44 @@
 require 'rails_helper'
 
 describe FavoritesItem, type: :model do
-  # it_behaves_like 'elasticable'
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:favorites_item, favorite: favorite, kind: :_id) }
+
+  let(:favorite) { create(:favorite, kind: :offers) }
+
+  it { is_expected.to belong_to(:favorite).counter_cache(true).touch(true) }
+  it { is_expected.to validate_presence_of(:kind) }
+
+  it {
+    expect(subject).to define_enum_for(:kind).with_values(
+      %i[users posts transactions accounts checks feeds post_categories advertiser_id feed_id feed_category_id _id]
+    )
+  }
+
+  shared_examples_for 'valid kinds' do |favorites_item_kind, favorite_kind|
+    subject { build(:favorites_item, kind: favorites_item_kind, favorite: create(:favorite, kind: favorite_kind)) }
+
+    it { is_expected.to be_valid }
+  end
+
+  shared_examples_for 'invalid kinds' do |favorites_item_kind, favorite_kind|
+    subject { build(:favorites_item, kind: favorites_item_kind, favorite: create(:favorite, kind: favorite_kind)) }
+
+    it { is_expected.not_to be_valid }
+  end
+
+
+  it_behaves_like 'valid kinds', 'accounts', 'accounts'
+  it_behaves_like 'valid kinds', 'feeds', 'feeds'
+  it_behaves_like 'valid kinds', 'posts', 'posts'
+  it_behaves_like 'valid kinds', 'transactions', 'transactions'
+  it_behaves_like 'valid kinds', 'checks', 'checks'
+  it_behaves_like 'valid kinds', 'post_categories', 'post_categories'
+  it_behaves_like 'valid kinds', 'advertiser_id', 'offers'
+  it_behaves_like 'valid kinds', 'feed_id', 'offers'
+  it_behaves_like 'valid kinds', 'feed_category_id', 'offers'
+  it_behaves_like 'valid kinds', '_id', 'offers'
+
+  it_behaves_like 'invalid kinds', 'accounts', 'feeds'
+  it_behaves_like 'invalid kinds', '_id', 'accounts'
+  it_behaves_like 'invalid kinds', 'transactions', 'offers'
 end
