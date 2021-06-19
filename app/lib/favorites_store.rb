474a4438e4a6a8b319e65b5ee61e5e02d3ca498c
Ruslan <oganer@gmail.com>
2021-06-19 20:16:28 +0300

    feat: commit favorites_store.rb

--- app/lib/favorites_store.rb
+++ app/lib/favorites_store.rb
@@ -1,24 +1,49 @@
 # frozen_string_literal: true
 
 class FavoritesStore
-  attr_accessor :items
-
-  def initialize(current_user, ext_ids, favorite_kind, favorite_item_kind)
-    unless ext_ids.empty?
-      # NOTE: We group by ext_id for getting know if requested ext_id exist at
-      # least exists in one favorite list. That means that we will highlight it's star.
-      self.items = FavoritesItemPolicy::Scope.new(current_user, FavoritesItem)
-                                             .resolve
-                                             .select('DISTINCT ON (ext_id) ext_id')
-                                             .where(
-                                               favorites: { kind: favorite_kind },
-                                               kind: favorite_item_kind,
-                                               ext_id: ext_ids
-                                             )
-    end
+  def initialize(current_user)
+    @current_user = current_user
+    @searched_items = []
+    super()
   end
 
-  def find(needle_id)
-    items.any? { |fi| fi.ext_id == needle_id.to_s }
+  def append(ext_id, favorite_item_kind)
+    @searched_items << { ext_id: ext_id, favorite_item_kind: favorite_item_kind }
+  end
+
+  def find(needle_id, needle_favorite_item_kind)
+    favorite_item_kind = FavoritesItem.kinds[needle_favorite_item_kind.to_s]
+    raise "kind `#{needle_favorite_item_kind.inspect}` is not defined in FavoritesItem" unless favorite_item_kind
+
+    execute_query.any? { |fi| fi.ext_id == needle_id.to_s && fi.favorite_item_kind == favorite_item_kind }
+  end
+
+  private
+
+  def execute_query
+    return @execute_query if defined?(@execute_query)
+
+    @execute_query = []
+    grouped_items = @searched_items.group_by { |item| item[:favorite_item_kind] }
+    relation = FavoritesItemPolicy::Scope.new(@current_user, FavoritesItem).resolve
+
+    # NOTE: We group by favorites_items.ext_id and favorites_items.kind for getting know if requested ext_id at
+    # least exists in one favorite list. That means that we will highlight it's star.
+    query = relation.select('DISTINCT ON (ext_id, favorites_items.kind) ext_id, favorites_items.kind as favorite_item_kind')
+
+    wheres = grouped_items.map do |favorite_item_kind, ext_ids|
+      {
+        kind: favorite_item_kind,
+        ext_id: ext_ids.map { _1[:ext_id] }
+      }
+    end
+
+    query.where!(wheres[0])
+
+    wheres[1..].each do |where|
+      query.public_send(:or!, relation.where(where))
+    end
+
+    @execute_query = query.to_a
   end
 end
--- spec/lib/favorites_store_spec.rb
+++ spec/lib/favorites_store_spec.rb
@@ -3,34 +3,61 @@
 require 'rails_helper'
 
 describe FavoritesStore do
-  it 'finds only specific favorites' do
-    favorites_item = create(:favorites_item)
-    favorites_store = described_class.new(favorites_item.favorite.user,
-                                          favorites_item.ext_id,
-                                          favorites_item.favorite.kind,
-                                          favorites_item.kind)
+  subject(:favorites_store) { described_class.new(user) }
 
-    expect(favorites_store.find(favorites_item.ext_id)).to be_truthy
-    expect(favorites_store.find(Faker::Alphanumeric.alphanumeric)).to be_falsey
+  let(:user) { create(:user) }
+
+  describe 'different `kinds`' do
+    before do
+      subject.append(favorites_item.ext_id, favorites_item.kind)
+    end
+
+    let(:favorites_item) { create(:favorites_item, ext_id: 'a1', kind: :users, user: user) }
+
+    context 'when `favorites_items.kind` is an `:users` and `subject.find(..., :users)`' do
+      it 'finds' do
+        expect(subject.find(favorites_item.ext_id, :users)).to be_truthy
+      end
+    end
+
+    context 'when `favorites_items.kind` is an `:users` and `subject.find(..., :posts)`' do
+      it 'does not find' do
+        expect(subject.find(favorites_item.ext_id, :posts)).to be_falsey
+      end
+    end
   end
 
-  it 'issues correct SQL' do
-    user_id = Faker::Number.number(digits: 5)
-    favorites_item = build(:favorites_item)
-    favorites_store = described_class.new(user_id,
-                                          favorites_item.ext_id,
-                                          favorites_item.favorite.kind,
-                                          favorites_item.kind)
-    sql = %(
-      SELECT DISTINCT ON (ext_id) ext_id
-      FROM "favorites_items"
-      INNER JOIN "favorites" ON "favorites"."id" = "favorites_items"."favorite_id"
-      WHERE "favorites"."user_id" = #{user_id}
-        AND "favorites"."kind" = #{Favorite.kinds[favorites_item.favorite.kind]}
-        AND "favorites_items"."kind" = #{FavoritesItem.kinds[favorites_item.kind]}
-        AND "favorites_items"."ext_id" = '#{favorites_item.ext_id}'
-      ).squish
+  describe 'different `favorites_items.user`' do
+    before do
+      subject.append(favorites_item.ext_id, favorites_item.kind)
+    end
 
-    expect(favorites_store.items.to_sql).to(eq(sql))
+    context 'when favorites_item belongs to the user' do
+      let(:favorites_item) { create(:favorites_item, ext_id: 'a1', kind: :feeds, user: user) }
+
+      it 'finds' do
+        expect(subject.find(favorites_item.ext_id, :feeds)).to be_truthy
+      end
+    end
+
+    context 'when favorites_item does not belong to the user' do
+      let(:favorites_item) { create(:favorites_item, ext_id: 'a1', kind: :feeds, user: create(:user)) }
+
+      it 'does not find' do
+        expect(subject.find(favorites_item.ext_id, :feeds)).to be_falsey
+      end
+    end
+  end
+
+  it 'issues at most one SQL select query' do
+    favorites_item1 = create(:favorites_item)
+    favorites_item2 = create(:favorites_item)
+    subject.append(favorites_item1.ext_id, favorites_item1.kind)
+    subject.append(favorites_item2.ext_id, favorites_item2.kind)
+
+    expect do
+      subject.find(favorites_item1.ext_id, favorites_item1.kind)
+      subject.find(favorites_item2.ext_id, favorites_item2.kind)
+    end.not_to exceed_query_limit(1)
   end
 end
