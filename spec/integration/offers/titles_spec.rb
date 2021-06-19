291ebe9ab6f3e5675da5d1c68e9372c7d10ab099
Ruslan <oganer@gmail.com>
2021-06-20 01:04:28 +0300

    test: trifle

--- spec/integration/offers/titles_spec.rb
+++ spec/integration/offers/titles_spec.rb
@@ -9,7 +9,7 @@ describe Tables::OffersController, type: :system do
   let(:feed) { feed_category.feed }
   let(:advertiser) { feed.advertiser }
 
-  describe '/offers' do
+  describe 'GET /offers' do
     specify do
       visit offers_path(locale: :ru)
       expect(page).to have_title 'Все товары'
@@ -17,15 +17,15 @@ describe Tables::OffersController, type: :system do
     end
   end
 
-  describe '/advertisers/:id/offers' do
-    specify 'shows correct title' do
+  describe 'GET /advertisers/:advertiser_id/offers' do
+    specify do
       visit advertiser_offers_path(advertiser_id: advertiser.id, locale: :ru)
       expect(page).to have_title("Рекламодатель: #{advertiser.to_label}")
       expect(page).to have_css('h1', text: "Рекламодатель: #{advertiser.to_label}")
     end
   end
 
-  describe '/feeds/:id/offers' do
+  describe 'GET /feeds/:feed_id/offers' do
     specify do
       visit feed_offers_path(feed_id: feed.id, locale: :ru)
       expect(page).to have_title("Прайс лист: #{feed.to_label}")
@@ -33,7 +33,7 @@ describe Tables::OffersController, type: :system do
     end
   end
 
-  describe '/feed_categories/:id/offers' do
+  describe 'GET /feed_categories/:feed_category_id/offers' do
     specify do
       visit feed_category_offers_path(feed_category_id: feed_category.id, locale: :ru)
       expect(page).to have_title("Категория: #{feed_category.to_label}")
@@ -41,7 +41,7 @@ describe Tables::OffersController, type: :system do
     end
   end
 
-  describe '/offers?favorite_id=:id' do
+  describe 'GET /offers?favorite_id=:id' do
     specify do
       favorite = create(:favorite, kind: :offers)
       login_as(favorite.user, scope: :user)
