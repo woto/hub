55fd5000e5804ee8ea8d684ebb3220e551e6717d
Ruslan <oganer@gmail.com>
2021-06-24 23:51:19 +0300

    feat(homepage): add different links depending on user authentication

---
+++ spec/integration/homepage/index_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Tables::NewsController, type: :system do
+  context 'when user authenticated' do
+    it 'shows `Выбрать товар`' do
+      visit root_path(locale: :ru)
+      expect(page).to have_link('Выбрать товар', href: offers_path(locale: :ru))
+    end
+  end
+
+  context 'when user is not authenticated' do
+    it 'shows `Личный кабинет`' do
+      login_as(create(:user), scope: :user)
+      visit root_path(locale: :ru)
+      expect(page).to have_link('Личный кабинет', href: dashboard_path(locale: :ru))
+    end
+  end
+end
