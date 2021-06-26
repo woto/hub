6d480940665a2e8d4631ff7288140067e9c77120
Ruslan <oganer@gmail.com>
2021-06-26 16:05:09 +0300

    style: transactions/shared_columns_spec.rb

--- spec/integration/transactions/shared_columns_spec.rb
+++ spec/integration/transactions/shared_columns_spec.rb
@@ -6,8 +6,9 @@ describe 'Transactions shared columns', type: :system do
   it_behaves_like 'shared columns invisible by default' do
     let(:path) { transactions_path(locale: :ru) }
     let(:object) do
-      create(:transaction, debit: Account.for_user(user, :pending_post, :rub),
-                           credit: Account.for_subject('hub', :pending_post, :rub))
+      create(:transaction,
+             debit: Account.for_user(user, :pending_post, :rub),
+             credit: Account.for_subject('hub', :pending_post, :rub))
     end
     let(:column_id) { 'transaction_group_kind' }
     let(:select_title) { 'Transaction Group Kind' }
