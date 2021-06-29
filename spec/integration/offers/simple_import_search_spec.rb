80da71186963ff7e9209f2899923a7fe4a90fa79
Ruslan <oganer@gmail.com>
2021-06-30 00:37:31 +0300

    test: focus on simple import search spec

--- spec/integration/offers/simple_import_search_spec.rb
+++ spec/integration/offers/simple_import_search_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Offers page', type: :system do
+describe 'Offers page', type: :system, focus: true do
   let!(:feed) { create(:feed, xml_file_path: file_fixture('feeds/yml-simplified.xml')) }
 
   before do
