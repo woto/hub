441abef1573e5bc7fa4a232fc4ee17b45effd08e
Ruslan <oganer@gmail.com>
2021-07-03 22:35:45 +0300

    fix: news seeds
    News posts must have `:accrued_post` status

--- db/seeds/300_news.rb
+++ db/seeds/300_news.rb
@@ -11,18 +11,18 @@ def create_post_categories_random_tree(realm)
   end
 end
 
-user = FactoryBot.create(:user)
+user = FactoryBot.create(:user, role: :admin)
 
 I18n.available_locales.each do |locale|
   realm = Realm.pick(kind: :news, locale: locale)
 
   create_post_categories_random_tree(realm)
-  post_categories = PostCategory.leaves.where(realm: realm).order("RANDOM()")
+  post_categories = PostCategory.leaves.where(realm: realm).order('RANDOM()')
 
   20.times do
     Current.set(responsible: user) do
       post = FactoryBot.create(:post, realm: realm, user: user, post_category: post_categories.sample)
-      post.update!(status: :pending_post)
+      post.update!(status: :accrued_post)
     end
   end
 end
