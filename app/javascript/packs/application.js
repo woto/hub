3238e1fb6ae24462b711b2e9e3e18d2e66b5ff9b
Ruslan <oganer@gmail.com>
2021-07-04 03:11:59 +0300

    feat: replace tabler-icons with font awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "@fortawesome/fontawesome-free/css/all"
+
 // TODO: remove
 require("@rails/ujs").start()
 
