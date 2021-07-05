869d6a9915b01c8fa4d4bc7bd0956882455615d7
Ruslan <oganer@gmail.com>
2021-07-05 11:06:25 +0300

    test: add news_by_month_search_query_spec.rb

--- spec/queries/frames/news_by_month_search_query_spec.rb
+++ spec/queries/frames/news_by_month_search_query_spec.rb
@@ -3,5 +3,38 @@
 require 'rails_helper'
 
 describe Frames::NewsByMonthSearchQuery do
-  pending
+  subject { described_class.call(locale: locale) }
+
+  let(:locale) { :ru }
+
+  it 'builds correct query' do
+    freeze_time do
+      expect(subject.object).to eq(
+        _source: nil,
+        body: {
+          aggregations: {
+            group_by_month: {
+              date_histogram: {
+                field: 'published_at',
+                interval: 'month',
+                order: { _key: 'desc' }
+              }
+            }
+          },
+          query: {
+            bool: {
+              filter: [
+                { term: { "realm_kind.keyword": 'news' } },
+                { term: { "status.keyword": 'accrued_post' } },
+                { term: { "realm_locale.keyword": :ru } },
+                { range: { published_at: { lte: Time.current.utc } } }
+              ]
+            }
+          }
+        },
+        index: Elastic::IndexName.posts,
+        size: 0
+      )
+    end
+  end
 end
