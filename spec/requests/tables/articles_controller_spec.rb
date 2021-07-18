a79ee7aaccd643d9173891e5974be86417f60d96
Ruslan <oganer@gmail.com>
2021-07-19 00:21:16 +0300

    test: fix articles permissions' tests

--- spec/requests/tables/articles_controller_spec.rb
+++ spec/requests/tables/articles_controller_spec.rb
@@ -2,8 +2,8 @@
 
 require 'rails_helper'
 
-describe Tables::NewsController, type: :request do
-  let(:path) { news_index_path(cols: '', order: :desc, per: 20, sort: :id) }
+describe Tables::ArticlesController, type: :request do
+  let(:path) { articles_url(host: create(:realm).domain, cols: '', order: :desc, per: 20, sort: :id) }
 
   it_behaves_like 'shared get_index', :guest, :ok
   it_behaves_like 'shared get_index', :user, :ok
