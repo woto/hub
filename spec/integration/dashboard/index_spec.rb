0e0f4ac918f5b43689337b637cbd6730bf540799
Ruslan <oganer@gmail.com>
2021-07-19 00:05:23 +0300

    feat: fix link in news component

--- spec/integration/dashboard/index_spec.rb
+++ spec/integration/dashboard/index_spec.rb
@@ -3,16 +3,23 @@
 require 'rails_helper'
 
 describe DashboardController, type: :system do
-  context 'articles-latest turbo-frame' do
-    before do
-      Realm.pick(locale: :ru, kind: :news)
-      user = create(:user)
-      login_as(user, scope: :user)
-      visit '/ru/dashboard'
-    end
+  let!(:realm) { Realm.pick(locale: :ru, kind: :news) }
+  let(:user) { create(:user) }
 
+  before do
+    login_as(user, scope: :user)
+    visit '/ru/dashboard'
+  end
+
+  context 'articles-latest turbo-frame' do
     it 'renders articles-latest frame' do
       expect(page).to have_css('turbo-frame#articles-latest[src="/ru/frames/articles/latest"]')
+    end
+  end
+
+  describe 'link to the news' do
+    it 'has correct link to the news' do
+      expect(page).to have_link(href: articles_url(host: realm.domain, port: Capybara.current_session.server.port))
     end
   end
 end
