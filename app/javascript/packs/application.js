1c7c2c2553e62064ebbdda7b308658721f2f3022
Ruslan <oganer@gmail.com>
2021-07-21 03:38:44 +0300

    feat: remove action cable channels boilerplate

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,6 @@ require("@rails/ujs").start()
 import { Turbo, cable } from "@hotwired/turbo-rails"
 Turbo.setProgressBarDelay(0)
 
-require("channels")
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
