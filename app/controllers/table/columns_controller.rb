69d7927141fa2cadc783b875d0467b240e94bad3
Ruslan <oganer@gmail.com>
2021-07-24 02:44:56 +0300

    refactor: columns_controller.rb and workspaces_controller.rb
    Extracts common private methods to base_controller.rb

--- app/controllers/table/columns_controller.rb
+++ app/controllers/table/columns_controller.rb
@@ -1,28 +1,18 @@
 # frozen_string_literal: true
 
-class Table::ColumnsController < ApplicationController
-  skip_before_action :authenticate_user!
+module Table
+  class ColumnsController < BaseController
+    skip_before_action :authenticate_user!
 
-  def create
-    # seems not too secure
-    form_class = "Columns::#{params[:model].camelize.demodulize.singularize}Form".constantize
+    def create
+      # seems not too secure
+      form_class = "Columns::#{params[:model].camelize.demodulize.singularize}Form".constantize
 
-    strings = params[:columns_form][:displayed_columns]
-    ints = form_class.strings_to_ints(strings).join('.')
+      strings = params[:columns_form][:displayed_columns]
+      ints = form_class.strings_to_ints(strings).join('.')
 
-    url_opts = path_opts.merge(cols: ints)
-    redirect_to url_for(url_opts)
-  end
-
-  private
-
-  def path_opts
-    opts = request.params.slice(*GlobalHelper.workspace_params)
-    opts[:controller] = "/#{params[:model]}"
-    opts
-  end
-
-  def path
-    url_for(**path_opts, only_path: true)
+      url_opts = path_opts.merge(cols: ints)
+      redirect_to url_for(url_opts)
+    end
   end
 end
--- app/controllers/table/workspaces_controller.rb
+++ app/controllers/table/workspaces_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Table
-  class WorkspacesController < ApplicationController
+  class WorkspacesController < BaseController
     def create
       @workspace_form = policy_scope(Workspace).new(workspace_params)
       redirect_to(path) if @workspace_form.save
@@ -13,16 +13,6 @@ module Table
     end
 
     private
-
-    def path_opts
-      opts = request.params.slice(*GlobalHelper.workspace_params)
-      opts[:controller] = "/#{params[:model]}"
-      opts
-    end
-
-    def path
-      url_for(**path_opts, only_path: true)
-    end
 
     def workspace_params
       params.require(:workspace_form).permit(:name, :is_default)
