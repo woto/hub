33046af281a3c2a57fc6dc85986087dd6bcfe93a
Ruslan <oganer@gmail.com>
2021-07-24 02:24:31 +0300

    refactor: makes Table::ColumnsController looks like Table::WorkspacesController

--- app/controllers/table/columns_controller.rb
+++ app/controllers/table/columns_controller.rb
@@ -4,12 +4,25 @@ class Table::ColumnsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def create
-    url_opts = request.params.slice(*GlobalHelper.workspace_params)
-    url_opts[:controller] = "/#{params[:model]}"
-    ints = params[:columns_form][:displayed_columns]
     # seems not too secure
     form_class = "Columns::#{params[:model].camelize.demodulize.singularize}Form".constantize
-    url_opts[:cols] = form_class.strings_to_ints(ints).join('.')
+
+    strings = params[:columns_form][:displayed_columns]
+    ints = form_class.strings_to_ints(strings).join('.')
+
+    url_opts = path_opts.merge(cols: ints)
     redirect_to url_for(url_opts)
+  end
+
+  private
+
+  def path_opts
+    opts = request.params.slice(*GlobalHelper.workspace_params)
+    opts[:controller] = "/#{params[:model]}"
+    opts
+  end
+
+  def path
+    url_for(**path_opts, only_path: true)
   end
 end
