eea992b9c3b572405fc6d43b33a62f2dbe55ce31
Ruslan <oganer@gmail.com>
2021-07-31 05:45:38 +0300

    test: fix spec/requests/table/workspaces_controller_spec.rb

--- spec/requests/table/workspaces_controller_spec.rb
+++ spec/requests/table/workspaces_controller_spec.rb
@@ -6,35 +6,41 @@ describe Table::WorkspacesController do
   describe '#create' do
     subject { post(table_workspaces_path, params: params, headers: headers) }
 
-    context 'when params is valid' do
+    let(:headers) { { 'ACCEPT' => 'text/vnd.turbo-stream.html, text/html, application/xhtml+xml' } }
+
+    context 'when params are valid' do
       let(:params) do
         {
-          q: 'q',
-          per: 'per',
-          page: 'page',
-          sort: 'sort',
-          order: 'order',
-          cols: 'cols',
-          workspace_form: { name: Faker::Lorem.word, is_default: Faker::Boolean.boolean },
-          model: 'tables/feeds'
+          workspace_form: {
+            model: 'feeds',
+            state: {
+              q: 'q',
+              per: 'per',
+              page: 'page',
+              sort: 'sort',
+              order: 'order',
+              columns: ['id']
+            }.to_json,
+            name: Faker::Lorem.word,
+            is_default: Faker::Boolean.boolean
+          }
         }
       end
-
-      let(:headers) { { 'ACCEPT' => 'text/vnd.turbo-stream.html, text/html, application/xhtml+xml' } }
 
       it 'saves workspace and redirects to proper path' do
         sign_in(create(:user))
         expect do
           expect(subject).to(
-            redirect_to('http://www.example.com/en/feeds?cols=cols&order=order&page=page&per=per&q=q&sort=sort')
+            redirect_to(
+              feeds_url(columns: ['id'], order: 'order', page: 'page', per: 'per', q: 'q', sort: 'sort')
+            )
           )
         end.to change(Workspace, :count).by(1)
       end
     end
 
-    context 'when params is not valid' do
-      let(:params) { { workspace_form: { foo: :bar }, model: 'tables/feeds' } }
-      let(:headers) { { 'ACCEPT' => 'text/vnd.turbo-stream.html, text/html, application/xhtml+xml' } }
+    context 'when params are not valid' do
+      let(:params) { { workspace_form: { model: 'feeds', state: {}.to_json, foo: :bar } } }
 
       it 'replaces form with turbo stream and does not create workspace' do
         sign_in(create(:user))
