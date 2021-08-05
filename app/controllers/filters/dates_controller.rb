eeaa700a4ae6167da520fa77abc50830ce78a70b
Ruslan <oganer@gmail.com>
2021-08-06 01:27:48 +0300

    refactor: remove dates_controller.rb

--- app/controllers/filters/dates_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Filters::DatesController < ApplicationController
-  # def index
-  #   respond_to do |format|
-  #     format.js { render 'index' }
-  #   end
-  # end
-end
