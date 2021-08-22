30b5c47831247710c321c498ce5c82d7ee1eeb79
Ruslan <oganer@gmail.com>
2021-08-23 02:13:33 +0300

    feat: trackers changes
    * rename TAWK_ENABLED -> TRACKERS_ENABLED.
    * add condition for rendering trackers (tawk, google, yandex)
    * move tawk code from component to view partial

--- app/components/tawk_component.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class TawkComponent < ViewComponent::Base
-  def render?
-    ActiveModel::Type::Boolean.new.cast(ENV['TAWK_ENABLED'])
-  end
-end
--- spec/integration/dashboard/tawk_spec.rb
+++ spec/integration/dashboard/tawk_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-describe TawkComponent do
-  context "when TAWK_ENABLED == 'true'" do
+describe 'Tawk tracker' do
+  context "when TRACKERS_ENABLED == 'true'" do
     before do
-      stub_const('ENV', ENV.to_hash.merge('TAWK_ENABLED' => 'true'))
+      stub_const('ENV', ENV.to_hash.merge('TRACKERS_ENABLED' => 'true'))
     end
 
     it 'shows tawk widget' do
@@ -14,9 +14,9 @@ describe TawkComponent do
     end
   end
 
-  context "when TAWK_ENABLED == 'false'" do
+  context "when TRACKERS_ENABLED == 'false'" do
     before do
-      stub_const('ENV', ENV.to_hash.merge('TAWK_ENABLED' => 'false'))
+      stub_const('ENV', ENV.to_hash.merge('TRACKERS_ENABLED' => 'false'))
     end
 
     it 'does not show tawk widget' do
