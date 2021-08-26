48180b5618b40419a7cdd938eeb12a54bb6740cf
Ruslan <oganer@gmail.com>
2021-08-27 00:47:48 +0300

    style: rubocop

--- app/controllers/ajax/post_categories/leaves_controller.rb
+++ app/controllers/ajax/post_categories/leaves_controller.rb
@@ -1,44 +1,48 @@
 # frozen_string_literal: true
 
-class Ajax::PostCategories::LeavesController < ApplicationController
-  def index
-    authorize %i[ajax post_categories]
+module Ajax
+  module PostCategories
+    class LeavesController < ApplicationController
+      def index
+        authorize %i[ajax post_categories]
 
-    body = {
-      query: {
-        bool: {
-          must: {
-            multi_match: {
-              query: params[:q],
-              type: 'bool_prefix',
-              fields: %w[
-                title.autocomplete
-                title.autocomplete._2gram
-                title.autocomplete._3gram
+        body = {
+          query: {
+            bool: {
+              must: {
+                multi_match: {
+                  query: params[:q],
+                  type: 'bool_prefix',
+                  fields: %w[
+                    title.autocomplete
+                    title.autocomplete._2gram
+                    title.autocomplete._3gram
+                  ]
+                }
+              },
+              filter: [
+                {
+                  term: {
+                    leaf: true
+                  }
+                },
+                {
+                  term: {
+                    realm_id: params[:realm_id]
+                  }
+                }
               ]
             }
           },
-          filter: [
-            {
-              term: {
-                leaf: true
-              }
-            },
-            {
-              term: {
-                realm_id: params[:realm_id]
-              }
-            }
-          ]
+          size: 30
         }
-      },
-      size: 30
-    }
 
-    @categories = PostCategory.__elasticsearch__.search(body)
+        @categories = PostCategory.__elasticsearch__.search(body)
 
-    respond_to do |format|
-      format.json
+        respond_to do |format|
+          format.json
+        end
+      end
     end
   end
 end
