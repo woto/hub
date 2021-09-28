91d185bd72fa401301e45177bbc3c70e4537d38e
Ruslan <oganer@gmail.com>
2021-09-28 05:57:37 +0300

    test: add test for searching in post categories paths

--- spec/requests/ajax/post_categories/leaves_controller_spec.rb
+++ spec/requests/ajax/post_categories/leaves_controller_spec.rb
@@ -3,8 +3,6 @@
 require 'rails_helper'
 
 describe Ajax::PostCategories::LeavesController, responsible: :admin, type: :request do
-  subject { get ajax_post_categories_leaves_path(q: 'кат', realm_id: realm.id), xhr: true }
-
   let(:realm) { Realm.pick(locale: :ru, kind: :post) }
   let!(:parent) { create(:post_category, title: 'Родительская категория', realm: realm) }
   let!(:child) { create(:post_category, title: 'Дочерняя категория', realm: realm, parent: parent) }
@@ -14,13 +12,19 @@ describe Ajax::PostCategories::LeavesController, responsible: :admin, type: :req
     create(:post_category, title: 'Другая категория', realm: another_realm)
   end
 
-  it 'requires authentication' do
-    subject
-    expect(response).to have_http_status(:unauthorized)
-    expect(response.body).to eq('You need to sign in or sign up before continuing.')
+  context 'when user is not authorized' do
+    subject { get ajax_post_categories_leaves_path(q: 'кат', realm_id: realm.id), xhr: true }
+
+    it 'requires authentication' do
+      subject
+      expect(response).to have_http_status(:unauthorized)
+      expect(response.body).to eq('You need to sign in or sign up before continuing.')
+    end
   end
 
   context 'when user is authenticated' do
+    subject { get ajax_post_categories_leaves_path(q: 'кат', realm_id: realm.id), xhr: true }
+
     before { sign_in(Current.responsible) }
 
     it 'returns leaf categories' do
@@ -29,6 +33,18 @@ describe Ajax::PostCategories::LeavesController, responsible: :admin, type: :req
       expect(response.parsed_body).to eq(['id' => child.id.to_s,
                                           'path' => 'Родительская категория',
                                           'title' => 'Дочерняя категория'])
+    end
+
+    context 'when search query includes only in path and not in leaf category itself' do
+      subject { get ajax_post_categories_leaves_path(q: 'род', realm_id: realm.id), xhr: true }
+
+      it "finds leaf's category" do
+        subject
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body).to eq(['id' => child.id.to_s,
+                                            'path' => 'Родительская категория',
+                                            'title' => 'Дочерняя категория'])
+      end
     end
   end
 end
