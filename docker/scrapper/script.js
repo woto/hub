b128f5b0cb2dcb9d023925863e181d5335735d00
Ruslan Kornev <oganer@gmail.com>
2021-11-16 09:40:22 +0300

    feat: change default puppeteer headers because they contains info about headless browser

--- docker/scrapper/script.js
+++ docker/scrapper/script.js
@@ -116,6 +116,14 @@ app.get('/screenshot', async (request, response) => {
     const page = await browser.newPage();
     const url = new URL(urlString);
 
+    await page.setExtraHTTPHeaders({
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+      'upgrade-insecure-requests': '1',
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+      'accept-encoding': 'gzip, deflate, br',
+      'accept-language': 'en-US,en;q=0.9,en;q=0.8'
+    })
+
     try {
       await page.goto(urlString);
     } catch (e) {
