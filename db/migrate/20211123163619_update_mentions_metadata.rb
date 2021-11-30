0375575ffe01184eee8a515d5da5fc265edf96bb
Ruslan Kornev <oganer@gmail.com>
2021-11-30 16:46:12 +0300

    feat: remove data migration

--- db/migrate/20211123163619_update_mentions_metadata.rb
+++ db/migrate/20211123163619_update_mentions_metadata.rb
@@ -1,10 +1,9 @@
 class UpdateMentionsMetadata < ActiveRecord::Migration[6.1]
   def change
-    # TODO: remove after migration
-    Mention.find_each do |mention|
-      attacher = ImageUploader::Attacher.from_model(mention, :image)
-      attacher.refresh_metadata!
-      attacher.atomic_persist
-    end
+    # Mention.find_each do |mention|
+    #   attacher = ImageUploader::Attacher.from_model(mention, :image)
+    #   attacher.refresh_metadata!
+    #   attacher.atomic_persist
+    # end
   end
 end
