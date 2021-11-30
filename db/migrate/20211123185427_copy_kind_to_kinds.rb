aad720f52fa0b2083197f3e28eceb91d1498e93e
Ruslan Kornev <oganer@gmail.com>
2021-11-30 19:45:51 +0300

    feat: remove data mitration

--- db/migrate/20211123185427_copy_kind_to_kinds.rb
+++ db/migrate/20211123185427_copy_kind_to_kinds.rb
@@ -1,13 +1,12 @@
 class CopyKindToKinds < ActiveRecord::Migration[6.1]
   def change
-    # TODO: remove after migration
-    reversible do |dir|
-      dir.up do
-        Mention.find_each do |mention|
-          mention.kinds = [Mention::KINDS[mention.kind]]
-          mention.save!
-        end
-      end
-    end
+    # reversible do |dir|
+    #   dir.up do
+    #     Mention.find_each do |mention|
+    #       mention.kinds = [Mention::KINDS[mention.kind]]
+    #       mention.save!
+    #     end
+    #   end
+    # end
   end
 end
