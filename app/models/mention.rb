2b40c3a4ac49a6a7d86d337f2a6b8fedfa86e349
Ruslan Kornev <oganer@gmail.com>
2021-12-01 19:10:10 +0300

    feat: commit mention model

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -10,7 +10,7 @@
 #  kinds          :jsonb            not null
 #  published_at   :datetime
 #  sentiment      :integer          not null
-#  tags           :jsonb
+#  topics_count   :integer          default(0), not null
 #  url            :text
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
@@ -26,8 +26,7 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Mention < ApplicationRecord
-  # NOTE: do not change order until migrate
-  KINDS = %w[text image audio video].freeze
+  KINDS = %w[text image video audio].freeze
 
   has_logidze ignore_log_data: true
 
@@ -40,17 +39,24 @@ class Mention < ApplicationRecord
 
   belongs_to :user, counter_cache: true
 
-  has_many :entities_mentions
+  has_many :entities_mentions, dependent: :destroy
   has_many :entities, through: :entities_mentions, counter_cache: :entities_count
 
-  validates :entities, :url, :tags, :sentiment, :kinds, presence: true
-  validates :tags, length: { minimum: 2 }
-  validates :entities, length: { minimum: 1 }
+  has_many :mentions_topics, dependent: :destroy
+  has_many :topics, through: :mentions_topics, counter_cache: :topics_count
+
+  validates :topics, :image, :entities, :url, :sentiment, :kinds, presence: true
+  validates :entities, :topics, length: { minimum: 1 }
   validates :url, uniqueness: true
-  validates :image, presence: true
   validate :validate_kinds
 
-  before_destroy :stop_destroy
+  # before_destroy :stop_destroy
+
+  accepts_nested_attributes_for :topics, allow_destroy: true
+
+  # def topics_attributes=(val)
+  #   TODO: old or new topics
+  # end
 
   def as_indexed_json(_options = {})
     {
@@ -58,7 +64,7 @@ class Mention < ApplicationRecord
       kinds: kinds,
       published_at: published_at,
       sentiment: sentiment,
-      tags: tags,
+      topics: topics.map(&:to_label),
       url: url,
       created_at: created_at,
       updated_at: updated_at,
@@ -72,10 +78,10 @@ class Mention < ApplicationRecord
 
   private
 
-  def stop_destroy
-    errors.add(:base, :undestroyable)
-    throw :abort
-  end
+  # def stop_destroy
+  #   errors.add(:base, :undestroyable)
+  #   throw :abort
+  # end
 
   # TODO: find gem to avoid manual validation
   def validate_kinds
--- spec/models/mention_spec.rb
+++ spec/models/mention_spec.rb
@@ -10,7 +10,7 @@
 #  kinds          :jsonb            not null
 #  published_at   :datetime
 #  sentiment      :integer          not null
-#  tags           :jsonb
+#  topics_count   :integer          default(0), not null
 #  url            :text
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
@@ -35,18 +35,34 @@ RSpec.describe Mention, type: :model do
 
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
-    it { is_expected.to have_many(:entities_mentions) }
-    it { is_expected.to have_many(:entities).through(:entities_mentions) }
+    it { is_expected.to have_many(:entities_mentions).dependent(:destroy) }
+    it { is_expected.to have_many(:entities).through(:entities_mentions).counter_cache(:entities_count) }
+    it { is_expected.to have_many(:mentions_topics).dependent(:destroy) }
+    it { is_expected.to have_many(:topics).through(:mentions_topics).counter_cache(:topics_count) }
   end
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:entities) }
     it { is_expected.to validate_presence_of(:url) }
-    it { is_expected.to validate_presence_of(:tags) }
+    it { is_expected.to validate_presence_of(:topics) }
     it { is_expected.to validate_presence_of(:sentiment) }
     it { is_expected.to validate_presence_of(:kinds) }
-    it { is_expected.to validate_length_of(:tags).is_at_least(2) }
-    xit { is_expected.to validate_length_of(:entities).is_at_least(1) }
+
+    # https://github.com/thoughtbot/shoulda-matchers/issues/1007
+    # it { is_expected.to validate_length_of(:topics).is_at_least(1) }
+    describe '#topics minimal length' do
+      subject { build(:mention, topics: []).tap(&:valid?).errors.details }
+
+      it { is_expected.to eq({ topics: [{ error: :blank }, { count: 1, error: :too_short }] }) }
+    end
+
+    # https://github.com/thoughtbot/shoulda-matchers/issues/1007
+    # it { is_expected.to validate_length_of(:entities).is_at_least(1) }
+    describe '#entities minimal length' do
+      subject { build(:mention, entities: []).tap(&:valid?).errors.details }
+
+      it { is_expected.to eq({ entities: [{ error: :blank }, { count: 1, error: :too_short }] }) }
+    end
 
     describe '#validate_kinds' do
       subject { build(:mention, kinds: kinds) }
@@ -70,6 +86,8 @@ RSpec.describe Mention, type: :model do
       it { is_expected.to validate_uniqueness_of(:url) }
     end
   end
+
+  it { is_expected.to accept_nested_attributes_for(:topics).allow_destroy(true) }
 
   describe '#image' do
     subject { create(:mention) }
@@ -97,7 +115,7 @@ RSpec.describe Mention, type: :model do
         kinds: mention.kinds,
         published_at: mention.published_at&.utc,
         sentiment: mention.sentiment,
-        tags: mention.tags,
+        topics: mention.topics.map(&:to_label),
         url: mention.url,
         user_id: mention.user_id,
         image: be_a(String),
@@ -109,7 +127,7 @@ RSpec.describe Mention, type: :model do
     end
   end
 
-  describe '#stop_destroy', responsible: :admin do
+  xdescribe '#stop_destroy', responsible: :admin do
     context 'when mention is trying to destroy' do
       let(:mention) { create(:mention) }
 
@@ -119,6 +137,42 @@ RSpec.describe Mention, type: :model do
         expect(mention.errors.details).to eq({ base: [{ error: :undestroyable }] })
         expect(mention).to be_valid
       end
+    end
+  end
+
+  describe 'factory' do
+    subject! { create(:mention) }
+
+    specify do
+      expect(subject.topics.count).to eq(1)
+    end
+
+    specify do
+      expect(subject.entities.count).to eq(1)
+    end
+  end
+
+  context 'when removes mention with entity' do
+    subject! { create(:mention) }
+
+    it 'does not remove entity' do
+      expect do
+        expect do
+          subject.destroy
+        end.to change(described_class, :count).from(1).to(0)
+      end.not_to change(Entity, :count).from(1)
+    end
+  end
+
+  context 'when removes mention with topic' do
+    subject! { create(:mention) }
+
+    it 'does not remove topic' do
+      expect do
+        expect do
+          subject.destroy
+        end.to change(described_class, :count).from(1).to(0)
+      end.not_to change(Topic, :count).from(1)
     end
   end
 end
