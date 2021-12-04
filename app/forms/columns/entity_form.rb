0f0dcc56bac049acdd85477c6043386ddac43021
Ruslan Kornev <oganer@gmail.com>
2021-12-04 20:34:23 +0300

    feat: commit forms/columns

--- app/forms/columns/entity_form.rb
+++ app/forms/columns/entity_form.rb
@@ -2,14 +2,15 @@
 
 module Columns
   class EntityForm < BaseForm
-    DEFAULTS = %w[id image title aliases created_at].freeze
+    DEFAULTS = %w[id image title lookups created_at].freeze
 
     self.all_columns = [
       { key: 'id',                  pg: Entity.columns_hash['id'], roles: ['user', 'manager', 'admin'] },
       { key: 'image',               pg: Entity.columns_hash['image_data'], as: :string, roles: ['user', 'manager', 'admin'] },
       { key: 'user_id',             pg: Entity.columns_hash['user_id'], roles: ['manager', 'admin'] },
       { key: 'title',               pg: Entity.columns_hash['title'], roles: ['user', 'manager', 'admin'] },
-      { key: 'aliases',             pg: Entity.columns_hash['aliases'], as: :text, roles: ['user', 'manager', 'admin'] },
+      { key: 'lookups',             pg: Lookup.columns_hash['title'], roles: ['user', 'manager', 'admin'] },
+      { key: 'lookups_count',       pg: Entity.columns_hash['lookups_count'], roles: ['user', 'manager', 'admin'] },
       { key: 'mentions_count',      pg: Entity.columns_hash['mentions_count'], roles: ['user', 'manager', 'admin'] },
       { key: 'created_at',          pg: Entity.columns_hash['created_at'], roles: ['user', 'manager', 'admin'] },
       { key: 'updated_at',          pg: Entity.columns_hash['updated_at'], roles: ['user', 'manager', 'admin'] }
--- app/forms/columns/mention_form.rb
+++ app/forms/columns/mention_form.rb
@@ -11,11 +11,12 @@ module Columns
         { key: 'url',                 pg: Mention.columns_hash['url'], roles: ['user', 'manager', 'admin'] },
         { key: 'entities',            pg: Mention.columns_hash['id'], as: :string, roles: ['user', 'manager', 'admin'] },
         { key: 'entity_ids',          pg: Mention.columns_hash['id'], as: :integer, roles: ['user', 'manager', 'admin'] },
-        { key: 'tags',                pg: Mention.columns_hash['tags'], as: :string, roles: ['user', 'manager', 'admin'] },
+        { key: 'topics',              pg: Topic.columns_hash['title'], as: :string, roles: ['user', 'manager', 'admin'] },
         { key: 'published_at',        pg: Mention.columns_hash['published_at'], roles: ['user', 'manager', 'admin'] },
         { key: 'kinds',               pg: Mention.columns_hash['kinds'], as: :string, roles: %w[guest user manager admin] },
         { key: 'sentiment',           pg: Mention.columns_hash['sentiment'], as: :string, roles: %w[guest user manager admin] },
         { key: 'entities_count',      pg: Mention.columns_hash['entities_count'], roles: ['user', 'manager', 'admin'] },
+        { key: 'topics_count',        pg: Mention.columns_hash['topics_count'], as: :integer, roles: ['user', 'manager', 'admin'] },
         { key: 'created_at',          pg: Mention.columns_hash['created_at'], roles: ['user', 'manager', 'admin'] },
         { key: 'updated_at',          pg: Mention.columns_hash['updated_at'], roles: ['user', 'manager', 'admin'] },
     ]
