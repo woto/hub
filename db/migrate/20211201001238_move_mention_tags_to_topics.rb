75e374d1e05e8bf1b5a3b4a828781bdc9028799b
Ruslan Kornev <oganer@gmail.com>
2021-12-06 01:00:54 +0300

    feat: comment out migrations

--- db/migrate/20211201001238_move_mention_tags_to_topics.rb
+++ db/migrate/20211201001238_move_mention_tags_to_topics.rb
@@ -2,19 +2,18 @@
 
 class MoveMentionTagsToTopics < ActiveRecord::Migration[6.1]
   def change
-    # TODO: remove after migration
-    reversible do |dir|
-      dir.up do
-        Mention.find_each do |mention|
-          topics = []
-          mention.tags.each do |tag|
-            next if tag == ''
-
-            topics << Topic.find_or_create_by!(title: tag)
-          end
-          mention.update!(topics: topics)
-        end
-      end
-    end
+    # reversible do |dir|
+    #   dir.up do
+    #     Mention.find_each do |mention|
+    #       topics = []
+    #       mention.tags.each do |tag|
+    #         next if tag == ''
+    #
+    #         topics << Topic.find_or_create_by!(title: tag)
+    #       end
+    #       mention.update!(topics: topics)
+    #     end
+    #   end
+    # end
   end
 end
