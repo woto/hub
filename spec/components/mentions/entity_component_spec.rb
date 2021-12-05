288b6c571851473cd394355e6e6405b6b8bd4a9d
Ruslan Kornev <oganer@gmail.com>
2021-12-05 04:06:41 +0300

    test: fix locale in entity_component_spec.rb

--- spec/components/mentions/entity_component_spec.rb
+++ spec/components/mentions/entity_component_spec.rb
@@ -3,12 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Mentions::EntityComponent, type: :component do
+  around do |example|
+    I18n.with_locale(:ru) { example.run }
+  end
+
   it 'renders entities popover button' do
     expect(
       render_inline(described_class.new(entity: %w[title 555])).to_html
     ).to eq <<~HERE.strip
-      <div data-controller="popover" class="d-inline-block" data-popover-url-value="/en/entities/555/popover">
-        <a tabindex="0" class="btn btn-sm btn-info mb-1 me-1" data-popover-target="hoverableElement" href="/en/entities/555">
+      <div data-controller="popover" class="d-inline-block" data-popover-url-value="/ru/entities/555/popover">
+        <a tabindex="0" class="btn btn-sm btn-info mb-1 me-1" data-popover-target="hoverableElement" href="/ru/entities/555">
           title
       </a>
       </div>
