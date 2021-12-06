d17b7acbb1d64a53da6bc6c64e1bfe9c5f2ef16e
Ruslan Kornev <oganer@gmail.com>
2021-12-06 05:11:38 +0300

    feat: add smarter object search by autocompletion + pagination

--- app/interactors/interactors/mentions/entities.rb
+++ app/interactors/interactors/mentions/entities.rb
@@ -6,27 +6,33 @@ module Interactors
       include ApplicationInteractor
 
       def call
-        body = {
-          query: {
-            bool: {
-              must: {
-                multi_match: {
-                  query: context.q,
-                  type: 'bool_prefix',
-                  fields: %w[
-                    title.autocomplete
-                    title.autocomplete._2gram
-                    title.autocomplete._3gram
-                    lookups
-                  ]
-                }
-              }
-            }
-          },
-          size: 3
-        }
+        body = Jbuilder.new do |json|
+          json.query do
+            json.bool do
+              json.must do
+                json.multi_match do
+                  json.query context.q
+                  json.type 'bool_prefix'
+                  json.fields do
+                    json.array! %w[
+                      title
+                      title.autocomplete
+                      title.autocomplete._2gram
+                      title.autocomplete._3gram
+                      lookups
+                      lookups.autocomplete
+                      lookups.autocomplete._2gram
+                      lookups.autocomplete._3gram
+                    ]
+                  end
+                end
+              end
+            end
+          end
+        end
 
-        context.object = Entity.__elasticsearch__.search(body)
+        query = body.attributes!.deep_symbolize_keys
+        context.object = Entity.__elasticsearch__.search(query).page(context.page).limit(context.limit)
       end
     end
   end
