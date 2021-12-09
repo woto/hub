dd81066b137603bcd1c61e72ee1efa978f744027
Ruslan Kornev <oganer@gmail.com>
2021-12-09 13:53:15 +0300

    feat: make data-turbo: false in navbar menu optional

--- app/components/left/menu/item_component.rb
+++ app/components/left/menu/item_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Left::Menu::ItemComponent < ViewComponent::Base
-  def initialize(path:, current_page:, title:, icon:, disabled:)
+  def initialize(path:, current_page:, title:, icon:, disabled:, options:)
     @path = path
     @current_page = current_page
     @title = title
     @icon = icon
     @disabled = disabled
+    @options = options
   end
 end
