325fec18178215867388328e3b9f56778e3fc7ee
Ruslan Kornev <oganer@gmail.com>
2021-12-09 13:57:31 +0300

    fix: previous commit

--- app/components/left/menu/dropdown/item_component.rb
+++ app/components/left/menu/dropdown/item_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Left::Menu::Dropdown::ItemComponent < ViewComponent::Base
-  def initialize(title:, path:, current_page:, disabled:)
+  def initialize(title:, path:, current_page:, disabled:, options:)
     @title = title
     @path = path
     @current_page = current_page
     @disabled = disabled
+    @options = options
   end
 end
