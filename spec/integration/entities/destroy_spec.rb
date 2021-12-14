498d35d0b6de64ce1d162704c43e545506f87cdf
Ruslan Kornev <oganer@gmail.com>
2021-12-14 09:21:43 +0300

    test: fix entities/destroy_spec.rb

--- spec/integration/entities/destroy_spec.rb
+++ spec/integration/entities/destroy_spec.rb
@@ -3,20 +3,22 @@
 require 'rails_helper'
 
 describe EntitiesController, responsible: :admin, type: :system do
+  let!(:entity) { create(:entity) }
+  let(:title) { "№ #{entity.id}" }
+
   def open_page
     login_as(Current.responsible, scope: :user)
     visit entities_path(locale: 'ru')
   end
 
   context 'when entity has mentions' do
-    before do
-      create(:mention, entities: [create(:entity)])
-      open_page
-    end
+    let!(:mention) { create(:mention, entities: [entity]) }
 
     it "can't be destroyed" do
+      open_page
+
       expect do
-        click_on('Управление')
+        click_on(title)
         accept_confirm { click_on('Удалить') }
         expect(page).to have_text('Невозможно удалить запись, так как существуют зависимости: entities mentions')
       end.to not_change(Entity, :count).and not_change(EntitiesMention, :count)
@@ -24,14 +26,11 @@ describe EntitiesController, responsible: :admin, type: :system do
   end
 
   context 'when entity does not have mentions' do
-    before do
-      create(:entity)
-      open_page
-    end
-
     it 'can be destroyed' do
+      open_page
+
       expect do
-        click_on('Управление')
+        click_on(title)
         accept_confirm { click_on('Удалить') }
         expect(page).to have_text('Объект упоминания был успешно удален')
       end.to change(Entity, :count).by(-1)
