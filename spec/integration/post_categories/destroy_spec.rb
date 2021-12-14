cfd78726e85642ca07cd8e8dc75d001df9c62f7a
Ruslan Kornev <oganer@gmail.com>
2021-12-14 09:46:25 +0300

    test: fix post_categoris/destroy_spec.rb

--- spec/integration/post_categories/destroy_spec.rb
+++ spec/integration/post_categories/destroy_spec.rb
@@ -4,7 +4,8 @@ require 'rails_helper'
 
 describe PostCategoriesController, responsible: :admin, type: :system do
   let!(:post_category) { create(:post_category, realm: realm) }
-  let!(:realm) { create(:realm, kind: :post) }
+  let(:realm) { create(:realm, kind: :post) }
+  let(:title) { "№ #{post_category.id}" }
 
   before do
     login_as(Current.responsible, scope: :user)
@@ -18,7 +19,7 @@ describe PostCategoriesController, responsible: :admin, type: :system do
 
     it "can't be destroyed" do
       expect do
-        click_on('Управление')
+        click_on(title)
         accept_confirm { click_on('Удалить') }
         expect(page).to have_text('Невозможно удалить запись, так как существуют зависимости: posts')
       end.not_to change(PostCategory, :count)
@@ -32,7 +33,7 @@ describe PostCategoriesController, responsible: :admin, type: :system do
 
     it "can't be destroyed" do
       expect do
-        click_on('Управление')
+        click_on(title)
         accept_confirm { click_on('Удалить') }
         expect(page).to have_text('Удаляемая категория не должна иметь дочерних категорий')
       end.not_to change(PostCategory, :count)
@@ -42,7 +43,7 @@ describe PostCategoriesController, responsible: :admin, type: :system do
   context 'when post_category does not have children associations' do
     it 'can be destroyed' do
       expect do
-        click_on('Управление')
+        click_on(title)
         accept_confirm { click_on('Удалить') }
         expect(page).to have_text('Категория статей была успешно удалена')
       end.to change(PostCategory, :count).by(-1)
