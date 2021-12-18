c6d51bd56651f7f78303d83ef05681d65ab869e2
Ruslan Kornev <oganer@gmail.com>
2021-12-18 19:52:07 +0300

    test: fix some tests

--- spec/jobs/sitemap_job_spec.rb
+++ spec/jobs/sitemap_job_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SitemapJob, type: :job do
-  subject { described_class.perform_now }
+  subject { described_class.perform_now('zzz') }
 
   it 'works' do
-    expect(SitemapGenerator::Interpreter).to receive(:run)
+    expect(SitemapGenerator::Interpreter).to receive(:run).with({ config_file: File.join(Rails.root, 'config/zzz') })
     subject
   end
 end
--- spec/requests/robots_spec.rb
+++ spec/requests/robots_spec.rb
@@ -1,19 +1,41 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Robots", type: :request do
-  describe "GET /robots.txt" do
-    let(:realm) { create(:realm) }
+RSpec.describe 'Robots', type: :request do
+  context 'when site is a realm' do
+    describe 'GET /robots.txt' do
+      let(:realm) { create(:realm) }
 
-    before do
-      get robots_url(host: realm.domain)
+      before do
+        # get robots_url(host: realm.domain)
+        get "https://#{realm.domain}/robots.txt"
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns robots.txt with valid sitemap' do
+        expect(response.body).to eq("Sitemap: https://#{realm.domain}/sitemaps/#{realm.domain}/sitemap.xml.gz\n")
+      end
     end
+  end
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+  context 'when site is a hub' do
+    describe 'GET /robots.txt' do
+      before do
+        # get robots_url(host: realm.domain)
+        get "#{root_url}robots.txt"
+      end
 
-    it 'returns robots.txt with valid sitemap' do
-      expect(response.body).to eq("Sitemap: http://#{realm.domain}/sitemaps/#{realm.domain}/sitemap.xml.gz\n")
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns robots.txt with valid sitemap' do
+        expect(response.body).to eq("Sitemap: #{root_url}sitemaps/sitemap.xml.gz\n")
+      end
     end
   end
 end
