fed04c617f15a53dfc09490fb449893e345e0b6f
Ruslan Kornev <oganer@gmail.com>
2021-12-21 21:32:42 +0300

    feat: add api/tools/metadata

--- app/lib/api/tools.rb
+++ app/lib/api/tools.rb
@@ -7,6 +7,18 @@ module API
     prefix :api
 
     resource :tools do
+
+      desc 'Extract metadata from page'
+
+      params do
+        requires :url, type: String, desc: 'Page URL'
+      end
+
+      # TODO: to test
+      get :metadata do
+        ExtractMetadata.call(url: params[:url]).object
+      end
+
       desc 'Detect a language of passed string'
 
       params do
