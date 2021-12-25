b1968dee39765922062dfa608f6f91e00d587d68
Ruslan Kornev <oganer@gmail.com>
2021-12-25 04:31:41 +0300

    feat: comment out data migration

--- db/migrate/20211214164505_copy_sentiment_to_sentiments.rb
+++ db/migrate/20211214164505_copy_sentiment_to_sentiments.rb
@@ -2,15 +2,14 @@
 
 class CopySentimentToSentiments < ActiveRecord::Migration[6.1]
   def change
-    # TODO: remove after migration
-    reversible do |dir|
-      dir.up do
-        Mention.find_each do |mention|
-          val = Mention::SENTIMENTS[mention.sentiment]
-          mention.sentiments = val ? [val] : []
-          mention.save!
-        end
-      end
-    end
+    # reversible do |dir|
+    #   dir.up do
+    #     Mention.find_each do |mention|
+    #       val = Mention::SENTIMENTS[mention.sentiment]
+    #       mention.sentiments = val ? [val] : []
+    #       mention.save!
+    #     end
+    #   end
+    # end
   end
 end
