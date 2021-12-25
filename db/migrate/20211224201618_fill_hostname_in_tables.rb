30f9714aa277cc8ec8c8c5ea98eba225b1d97dfa
Ruslan Kornev <oganer@gmail.com>
2021-12-25 07:41:38 +0300

    feat: comment out hostname data migration

--- db/migrate/20211224201618_fill_hostname_in_tables.rb
+++ db/migrate/20211224201618_fill_hostname_in_tables.rb
@@ -1,7 +1,6 @@
 class FillHostnameInTables < ActiveRecord::Migration[6.1]
   def change
-    # TODO: remove later
-    Mention.find_each(&:save)
-    Entity.find_each(&:save)
+    # Mention.find_each(&:save)
+    # Entity.find_each(&:save)
   end
 end
