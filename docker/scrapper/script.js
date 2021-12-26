f34f17727fe541e8cc9192dd229e0b67c901f85a
Ruslan Kornev <oganer@gmail.com>
2021-12-26 04:53:42 +0300

    fix: dev.to scrape and commented out one page.goto

--- docker/scrapper/script.js
+++ docker/scrapper/script.js
@@ -55,14 +55,15 @@ app.get('/screenshot', async (request, response) => {
         const url = new URL(urlString);
 
         await page.setExtraHTTPHeaders({
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
             'upgrade-insecure-requests': '1',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'en-US,en;q=0.9,en;q=0.8'
         })
 
         try {
+            // await page.goto(urlString);
             await page.goto(urlString);
             await page.goto(urlString, {waitUntil: "networkidle2"});
         } catch (e) {
