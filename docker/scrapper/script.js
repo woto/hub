a205b626ea0c05ecc65f3af40553dfc1bd6d6445
Ruslan Kornev <oganer@gmail.com>
2022-02-12 17:26:45 +0300

    feat[scrapper]: close page instead of browser

--- docker/scrapper/script.js
+++ docker/scrapper/script.js
@@ -31,72 +31,79 @@ async function takeScreenshotWithDelay(page) {
     }
 }
 
-app.get('/screenshot', async (request, response) => {
+(async () => {
 
-    const browser = await puppeteer.launch({
-        // dumpio: true,
-        defaultViewport: {width: 1280, height: 1280},
-        // headless: false,
-        args: [
-            // Required for Docker version of Puppeteer
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            // This will write shared memory files into /tmp instead of /dev/shm,
-            // because Docker’s default for /dev/shm is 64MB
-            '--disable-dev-shm-usage',
-            // Disable CORS policy
-            '--disable-web-security'
-        ]
-    })
+  const browser = await puppeteer.launch({
+      // dumpio: true,
+      defaultViewport: {width: 1280, height: 1280},
+      // headless: false,
+      args: [
+          // Required for Docker version of Puppeteer
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          // This will write shared memory files into /tmp instead of /dev/shm,
+          // because Docker’s default for /dev/shm is 64MB
+          '--disable-dev-shm-usage',
+          // Disable CORS policy
+          '--disable-web-security'
+      ]
+  })
 
-    try {
-        console.log('started');
-        const urlString = request.query.url
+  app.get('/screenshot', async (request, response) => {
 
-        const page = await browser.newPage();
-        const url = new URL(urlString);
+      const page = await browser.newPage();
 
-        await page.setExtraHTTPHeaders({
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-            'upgrade-insecure-requests': '1',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9,en;q=0.8'
-        })
+      try {
+          const urlString = request.query.url
 
-        try {
-            // await page.goto(urlString);
-            // await page.goto(urlString);
-            let response = await page.goto(urlString, {waitUntil: "networkidle2", timeout: 60000});
-            if(!response.ok()) {
-              throw Error('Response is not successfull')
-            }
-        } catch (e) {
-            throw Error('Unable to goto to the url')
-        }
+          const url = new URL(urlString);
 
-        const image = await takeScreenshotWithDelay(page);
-        const html = await page.content();
+          await page.setExtraHTTPHeaders({
+              'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+              'upgrade-insecure-requests': '1',
+              'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+              'accept-encoding': 'gzip, deflate, br',
+              'accept-language': 'en-US,en;q=0.9,en;q=0.8'
+          })
 
-        var doc = new JSDOM(html, {url: urlString});
-        let reader = new Readability(doc.window.document);
-        let article = reader.parse();
+          try {
+              let response = await page.goto(urlString);
+              // let response = await page.goto(urlString, {waitUntil: "networkidle2", timeout: 60000});
+              if(!response.ok()) {
+                throw Error('Response is not successfull')
+              }
+          } catch (e) {
+              console.error(e);
+              throw Error('Unable to goto to the url')
+          }
 
-        const title = await page.title();
+          const image = await takeScreenshotWithDelay(page);
+          const html = await page.content();
 
-        response.send({
-            image: `data:image/png;base64, ${image}`,
-            html: html,
-            title: title,
-            article: article
-        })
-    } catch (e) {
-        console.error(e);
-        response.status(400);
-        response.send({error: e.message})
-    } finally {
-        await browser.close();
-    }
-})
+          var doc = new JSDOM(html, {url: urlString});
+          let reader = new Readability(doc.window.document);
+          let article = reader.parse();
+
+          const title = await page.title();
+
+          console.log(`succeeded ${request.query.url}`);
+
+          response.send({
+              image: `data:image/png;base64, ${image}`,
+              html: html,
+              title: title,
+              article: article
+          })
+      } catch (e) {
+          // console.error(e);
+          console.error(`failed ${request.query.url}`);
+          response.status(400);
+          response.send({error: e.message})
+      } finally {
+          await page.close();
+      }
+  })
+})();
+
 
 app.listen(4000)
