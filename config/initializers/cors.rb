627e9e26e91c9da297faf98dcb12c5564b746903
Ruslan Kornev <oganer@gmail.com>
2022-06-12 05:23:42 +0300

    feat: add CORS rule for /upload/images

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -7,10 +9,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins '*'
 
-    resource "/api/*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '/api/*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+
+    resource '/images/upload',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
