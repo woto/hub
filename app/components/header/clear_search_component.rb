02b2fad253ab4277071c113e60771490bfb29017
Ruslan Kornev <oganer@gmail.com>
2022-06-29 12:46:26 +0300

    feat: replace search bar with React component

--- app/components/header/clear_search_component.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class Header::ClearSearchComponent < ViewComponent::Base
-end
--- spec/shared/clear_search.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-shared_examples 'clear_search' do
-  skip
-end
