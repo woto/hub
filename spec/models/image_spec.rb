46ff73e4274270263f4983a77da0aa9347906fac
Ruslan Kornev <oganer@gmail.com>
2022-07-31 13:17:57 +0300

    docs: add image model schema information

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: images
+#
+#  id         :bigint           not null, primary key
+#  image_data :jsonb
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_images_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 describe Image do
