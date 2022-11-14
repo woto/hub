bc5e84c88b1a8854660357058aaa3d53ba6ac58c
Ruslan Kornev <oganer@gmail.com>
2022-11-14 07:48:16 +0600

    feat: temporary disable animation in mentions list

--- app/javascript/react_components/Mentions/MentionsCard.tsx
+++ app/javascript/react_components/Mentions/MentionsCard.tsx
@@ -82,33 +82,27 @@ export default function MentionsCard(
 
   return (
 
-    <AnimatePresence mode="wait" presenceAffectsLayout>
-      { isInView
-        ? (
-          <motion.div
-            key={`real-${props.mentionId}`}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-            ref={changeableRef}
-          >
-            <div className="tw-py-3 2xl:tw-py-5 tw-max-w-xl tw-mx-auto">
-              <div className="tw-flex tw-rounded-lg tw-flex-col tw-space-y-5 tw-bg-slate-50">
+    isInView
+      ? (
+        <div
+          ref={changeableRef}
+        >
+          <div className="tw-py-3 2xl:tw-py-5 tw-max-w-xl tw-mx-auto">
+            <div className="tw-flex tw-rounded-lg tw-flex-col tw-space-y-5 tw-bg-slate-50">
 
-                <div className="tw-flex tw-shrink tw-p-3 2xl:tw-p-3.5 2xl:tw-text-lg tw-bg-slate-600
+              <div className="tw-flex tw-shrink tw-p-3 2xl:tw-p-3.5 2xl:tw-text-lg tw-bg-slate-600
                              hover:tw-bg-sky-800 tw-rounded-lg tw-text-base"
-                >
-                  <a href={props.url} className="tw-text-slate-50 tw-font-medium">
-                    <Title title={props.title} />
+              >
+                <a href={props.url} className="tw-text-slate-50 tw-font-medium">
+                  <Title title={props.title} />
                   &nbsp;
-                    <Url url={props.url} />
+                  <Url url={props.url} />
                   &nbsp;
-                    <Icon />
-                  </a>
-                </div>
+                  <Icon />
+                </a>
+              </div>
 
-                {props.publishedAt
+              {props.publishedAt
                 && (
                   <div className="tw-flex tw-items-center tw-gap-x-2 tw-text-gray-500">
                     <ClockIcon className="tw-w-5 tw-h-5" />
@@ -116,42 +110,35 @@ export default function MentionsCard(
                   </div>
                 )}
 
-                {props.topics.length > 0
+              {props.topics.length > 0
                 && <Multiple tags={props.topics} limit={10} textColor="tw-text-blue-800" bgColor="tw-bg-blue-100" linkify={false} />}
 
-                <div
-                  className="tw-flex tw-justify-end tw-flex-grow tw-relative tw-rounded-lg
+              <div
+                className="tw-flex tw-justify-end tw-flex-grow tw-relative tw-rounded-lg
                   test-pattern? tw-bg-slate-100 tw-items-stretch tw-w-full"
-                >
-                  <Image image={props.image} url={props.url} layoutGroupId={props.layoutGroupId} />
-                  <Interaction
-                    mention={{
-                      id: props.mentionId, url: props.url, title: props.title, image: props.image,
-                    }}
-                    entities={props.entities.slice(0, 1)}
-                  />
-                </div>
-
-                <Carousel items={props.entities} type="multiple" carouselId={props.mentionId} />
-
+              >
+                <Image image={props.image} url={props.url} layoutGroupId={props.layoutGroupId} />
+                <Interaction
+                  mention={{
+                    id: props.mentionId, url: props.url, title: props.title, image: props.image,
+                  }}
+                  entities={props.entities.slice(0, 1)}
+                />
               </div>
-            </div>
-          </motion.div>
-        ) : (
-          <div
-            key={`mock-${props.mentionId}`}
-            // initial={{ opacity: 0 }}
-            // animate={{ opacity: 1 }}
-            // exit={{ opacity: 1 }}
-            // transition={{ duration: 0 }}
-            className="tw-min-h-[500px] tw-pt-8 tw-pb-8"
-            ref={changeableRef}
-            style={{ height: lastKnownHeight }}
-          >
-            <div className="tw-h-full tw-max-w-xl tw-mx-auto" />
-          </div>
-        )}
-    </AnimatePresence>
 
+              <Carousel items={props.entities} type="multiple" carouselId={props.mentionId} />
+
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div
+          className="tw-min-h-[500px] tw-pt-8 tw-pb-8"
+          ref={changeableRef}
+          style={{ height: lastKnownHeight }}
+        >
+          <div className="tw-h-full tw-max-w-xl tw-mx-auto" />
+        </div>
+      )
   );
 }
