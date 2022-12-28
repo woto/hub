07a8ef4458d19faba5f36123915306365326c014
Ruslan Kornev <oganer@gmail.com>
2022-12-28 17:01:12 +0600

    feat: add smooth disappear to toast

--- app/javascript/react_components/Toast/Toast.tsx
+++ app/javascript/react_components/Toast/Toast.tsx
@@ -14,92 +14,76 @@ const draw = {
     opacity: 1,
     transition: {
       opacity: { duration: 1 },
-      pathLength: { duration: 5, bounce: 0 },
+      pathLength: { duration: 10, bounce: 0 },
     },
   },
 };
 
 export default function Toast(props: {
   children: React.ReactNode,
-  onDismiss: MouseEventHandler<HTMLDivElement> | undefined
+  remove: (id: number) => void,
+  id: number,
 }) {
-  const [show, setShow] = useState(true);
-  const [showCircle, setShowCircle] = useState(false);
-
   const controls = useAnimationControls();
 
   useEffect(() => {
     controls.start('visible');
-  }, [showCircle]);
+  }, [controls]);
 
   return (
-    <AnimatePresence>
-      { show && (
-      <motion.div
-        initial={{ opacity: 0, scale: 0.95 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0.95 }}
-        transition={{ duration: 0.150, scale: 1, ease: 'easeOut' }}
-        onAnimationComplete={() => setShowCircle(true)}
-      >
-        <div
-          onMouseEnter={() => { controls.stop(); }}
-          onMouseLeave={() => { controls.start('visible'); }}
-          className="tw-max-w-sm tw-w-full tw-bg-stone-50 tw-shadow-lg tw-rounded-lg tw-pointer-events-auto
-          tw-ring-1 tw-ring-black tw-ring-opacity-5 tw-overflow-hidden"
-        >
-          <div className="tw-p-4">
-            <div className="tw-flex tw-items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      layout
+      onMouseEnter={() => {
+        controls.stop();
+      }}
+      onMouseLeave={() => {
+        controls.start('visible');
+      }}
+      className="tw-max-w-sm tw-w-full tw-bg-white tw-shadow-lg tw-rounded-lg tw-pointer-events-auto tw-ring-1 tw-ring-black tw-ring-opacity-5 tw-overflow-hidden"
+    >
+      <div className="tw-p-4">
+        <div className="tw-flex tw-items-center">
+          {props.children}
 
-              {props.children}
+          <div className="tw-ml-4 tw-relative tw-p-1 tw-flex-shrink-0 tw-flex">
+            <button
+              type="button"
+              className="tw-bg-white tw-rounded-full tw-inline-flex tw-text-gray-400 hover:tw-text-gray-500 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2 focus:tw-ring-indigo-500"
+              onClick={() => {
+                props.remove(props.id);
+              }}
+            >
+              <svg
+                className="tw-h-16 tw-w-16 tw-absolute tw-left-1/2 -tw-translate-x-1/2 tw-top-1/2 -tw-translate-y-1/2"
+                viewBox="0 0 40 40"
+              >
+                <circle
+                  className="tw-stroke-2 tw-stroke-sky-50 tw-fill-transparent"
+                  cx="20"
+                  cy="20"
+                  r="10"
+                />
 
-              <div className="tw-ml-4 tw-relative tw-p-1 tw-flex-shrink-0 tw-flex">
-                <button
-                  type="button"
-                  className="tw-bg-stone-50 tw-rounded-full tw-inline-flex tw-text-gray-400 hover:tw-text-gray-500
-                focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2 focus:tw-ring-indigo-500"
-                  onClick={() => {
-                    setShow(false);
-                  }}
-                >
+                <motion.circle
+                  onAnimationComplete={() => props.remove(props.id)}
+                  className="tw-stroke-2 tw-stroke-sky-300 tw-fill-transparent"
+                  cx="20"
+                  cy="20"
+                  r="10"
+                  initial="hidden"
+                  animate={controls}
+                  variants={draw}
+                />
+              </svg>
 
-                  <AnimatePresence>
-                    {showCircle
-                    && (
-                    <svg
-                      className="tw-h-16 tw-w-16 tw-absolute tw-left-1/2 -tw-translate-x-1/2 tw-top-1/2 -tw-translate-y-1/2"
-                      viewBox="0 0 40 40"
-                    >
-                      <circle
-                        className="tw-stroke-2 tw-stroke-sky-50 tw-fill-transparent"
-                        cx="20"
-                        cy="20"
-                        r="10"
-                      />
-
-                      <motion.circle
-                        onAnimationComplete={() => setShow(false)}
-                        className="tw-stroke-2 tw-stroke-sky-300 tw-fill-transparent"
-                        cx="20"
-                        cy="20"
-                        r="10"
-                        initial="hidden"
-                        animate={controls}
-                        variants={draw}
-                      />
-                    </svg>
-                    )}
-                  </AnimatePresence>
-
-                  <XMarkIcon className="tw-h-5 tw-w-5" aria-hidden="true" />
-
-                </button>
-              </div>
-            </div>
+              <XMarkIcon className="tw-h-5 tw-w-5" aria-hidden="true" />
+            </button>
           </div>
         </div>
-      </motion.div>
-      ) }
-    </AnimatePresence>
+      </div>
+    </motion.div>
   );
 }
--- app/javascript/react_components/Toast/ToastManager.tsx
+++ app/javascript/react_components/Toast/ToastManager.tsx
@@ -1,18 +1,17 @@
-import React, { MouseEventHandler, ReactNode } from 'react';
-import { motion } from 'framer-motion';
+import React, {
+  MouseEventHandler, ReactNode, useCallback, useMemo,
+} from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import { ToastContextInterface, ToastType } from '../main';
 import Toast from './Toast';
 
 const Ctx = React.createContext({} as ToastContextInterface);
 
-// Styled Components
-// ==============================
-
 function ToastContainer(props: { children: React.ReactNode }) {
   return (
     <div
       aria-live="assertive"
-      className="tw-fixed tw-z-20 tw-inset-0 tw-flex tw-items-end tw-px-4 tw-py-6 tw-pointer-events-none sm:tw-p-6 sm:tw-items-start"
+      className="tw-fixed tw-inset-0 tw-flex tw-items-end tw-px-4 tw-py-6 tw-pointer-events-none sm:tw-p-6 sm:tw-items-start"
     >
       <div className="tw-w-full tw-flex tw-flex-col tw-items-center tw-space-y-4 sm:tw-items-end">
         {props.children}
@@ -21,56 +20,38 @@ function ToastContainer(props: { children: React.ReactNode }) {
   );
 }
 
-// const TmpToast = (props: { children: React.ReactNode, onDismiss: MouseEventHandler<HTMLDivElement> | undefined }) => (
-//   <div
-//     style={{
-//       background: "LemonChiffon",
-//       cursor: "pointer",
-//       fontSize: 14,
-//       margin: 10,
-//       padding: 10,
-//       display: 'flex'
-//     }}
-//     onClick={props.onDismiss}
-//   >
-//     <Toast></Toast>
-//     {props.children}
-//   </div>
-// );
-
-// Provider
-// ==============================
-
 let toastCount = 0;
 
 export function ToastProvider(props: { children: React.ReactNode }) {
   const [toasts, setToasts] = React.useState<ToastType[]>([]);
 
-  const add = (content: string | ReactNode) => {
-    const id = toastCount++;
+  const add = useCallback((content: string | ReactNode) => {
+    toastCount += 1;
+    const id = toastCount;
     const toast = { content, id };
     setToasts((prevToasts) => [...prevToasts, toast]);
-  };
+  }, []);
 
-  const remove = (id: number) => {
+  const remove = useCallback((id: number) => {
     const newToasts = toasts.filter((t: ToastType) => t.id !== id);
     setToasts(newToasts);
-  };
+  }, [toasts]);
 
-  // avoid creating a new fn on every render
-  const onDismiss = (id: number) => () => remove(id);
+  const value = useMemo(() => ({ add, remove }), [add, remove]);
 
   return (
-    <Ctx.Provider value={{ add, remove }}>
+    <Ctx.Provider value={value}>
       {props.children}
       <ToastContainer>
-        {toasts.map(({ content, id, ...rest }) => (
-          <Toast key={id} onDismiss={onDismiss(id)} {...rest}>
-            <div className="tw-ml-3 tw-font-medium tw-flex-1 tw-pt-0.5 tw-text-sm tw-text-gray-900">
-              {content}
-            </div>
-          </Toast>
-        ))}
+        <AnimatePresence>
+          {toasts.map(({ content, id }) => (
+            <Toast key={id} id={id} remove={remove}>
+              <div className="tw-ml-3 tw-font-medium tw-flex-1 tw-pt-0.5 tw-text-sm tw-text-gray-900">
+                {content}
+              </div>
+            </Toast>
+          ))}
+        </AnimatePresence>
       </ToastContainer>
     </Ctx.Provider>
   );
