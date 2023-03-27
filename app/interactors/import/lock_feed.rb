3b148ec6bdcd7d38d7576dfbf3ab745fc236986a
Ruslan Kornev <oganer@gmail.com>
2023-03-27 06:27:04 +0600

    fix: restore Sidekiq's  "identity" and "tid"

--- app/interactors/import/lock_feed.rb
+++ app/interactors/import/lock_feed.rb
@@ -3,7 +3,7 @@
 module Import
   class LockFeed
     include ApplicationInteractor
-    # include Sidekiq::Util
+    include Sidekiq::Component
 
     def call
       ActiveRecord::Base.transaction do
