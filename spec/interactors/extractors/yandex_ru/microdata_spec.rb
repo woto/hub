83203411413e3ce6ee770679d264b15cfb19ef2d
Ruslan Kornev <oganer@gmail.com>
2023-04-16 14:38:55 +0600

    test: fix yandex microdata test

--- spec/interactors/extractors/yandex_ru/microdata_spec.rb
+++ spec/interactors/extractors/yandex_ru/microdata_spec.rb
@@ -6,7 +6,7 @@ describe Extractors::YandexRu::Microdata do
   context 'with url parameter' do
     it 'proxies request to yandex.com' do
       stub_request(:get, 'https://validator-api.semweb.yandex.ru/v1.1/url_parser?apikey=yandex_microdata_key_value&id=&lang=ru&only_errors=false&pretty=true&url=https://example.com')
-        .to_return(status: 200, body: { a: 'b' }.to_json, headers: {})
+        .to_return(status: 200, body: { a: 'b' }.to_json, headers: { 'Content-Type' => 'application/json' })
       result = described_class.call(url: 'https://example.com').object
       expect(result).to eq({ 'a' => 'b' })
     end
@@ -16,7 +16,7 @@ describe Extractors::YandexRu::Microdata do
     it 'proxies request to yandex.com' do
       stub_request(:post, "https://validator-api.semweb.yandex.ru/v1.1/document_parser?apikey=yandex_microdata_key_value&id=&lang=ru&only_errors=false&pretty=true")
         .with(body: '<html></html>')
-        .to_return(status: 200, body: { a: 'b' }.to_json, headers: {})
+        .to_return(status: 200, body: { a: 'b' }.to_json, headers: { 'Content-Type' => 'application/json' })
 
       result = described_class.call(html: '<html></html>').object
       expect(result).to eq({ 'a' => 'b' })
