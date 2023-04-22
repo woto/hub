4d52fff27a737da6f3a322478d8cc87bfb494ac2
Ruslan Kornev <oganer@gmail.com>
2023-04-22 16:42:25 +0600

    fix: temporary restore MentionPolicy

---
+++ app/policies/mention_policy.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class MentionPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  # def permitted_attributes
+  #   attributes = [
+  #     :published_at, :url, :image, :title, :html,
+  #     { tags: [], advertiser_ext_ids: [], topics_attributes: [],
+  #       related_entities: %i[id is_main] }
+  #   ]
+  #   attributes.append(:user_id) if user.staff?
+  #   attributes
+  # end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  # def create?
+  #   true if user
+  # end
+  #
+  # def update?
+  #   return true if super
+  #
+  #   true if user && context.user == user
+  # end
+  #
+  # def destroy?
+  #   return true if super
+  #
+  #   true if user && context.user == user
+  # end
+end
---
+++ spec/policies/mention_policy_spec.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# NOTE: Responsible admin needed for creation in the right status.
+# Treat it as a stub. It does not affect to the tested parts.
+describe MentionPolicy, responsible: :admin do
+  subject { described_class }
+
+  xdescribe '#permitted_attributes' do
+    subject { described_class.new(user, nil).permitted_attributes }
+
+    let(:permitted_attributes) do
+      [:published_at, :url, :image, :title, :html,
+       { related_entities: %i[id is_main], tags: [], advertiser_ext_ids: [], topics_attributes: [] }]
+    end
+
+    context 'with user' do
+      let(:user) { create(:user) }
+      let(:user_permitted_attributes) { permitted_attributes }
+
+      it { is_expected.to match_array(user_permitted_attributes) }
+    end
+
+    context 'with admin' do
+      let(:user) { create(:user, role: :admin) }
+      let(:admin_permitted_attributes) { permitted_attributes.append(:user_id) }
+
+      it { is_expected.to match_array(admin_permitted_attributes) }
+    end
+  end
+
+  # everybody can index mentions
+  describe 'index?' do
+    permissions :index? do
+      it { is_expected.to permit(nil, nil) }
+    end
+  end
+
+  # authenticated users can create
+  xdescribe 'create?' do
+    let(:user) { create(:user) }
+
+    permissions :create? do
+      it { is_expected.to permit(user, nil) }
+    end
+  end
+
+  xcontext 'with user' do
+    let(:user) { create(:user) }
+    let(:policy_context) { create(:mention, user: user) }
+
+    permissions :update?, :show?, :destroy? do
+      it { is_expected.to permit(user, policy_context) }
+    end
+  end
+
+  xcontext 'with another user' do
+    let(:user) { create(:user) }
+    let(:policy_context) { create(:mention, user: user) }
+
+    permissions :update?, :destroy? do
+      it { is_expected.not_to permit(create(:user), policy_context) }
+    end
+  end
+
+  context 'with admin' do
+    let(:user) { create(:user) }
+    let(:policy_context) { create(:mention, user: user) }
+
+    permissions :update?, :show?, :destroy? do
+      it { is_expected.to permit(create(:user, role: :admin), policy_context) }
+    end
+  end
+end
