d98f07f03527b566cfc55b50726c2488ffe923c3
Ruslan Kornev <oganer@gmail.com>
2023-04-29 10:58:58 +0600

    chore: add missed ElastiEntityPresenter

---
+++ app/presenters/entities/elastic_entity_presenter.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Entities
+  class ElasticEntityPresenter
+    include ApplicationInteractor
+
+    # delegate :entity, to: :context
+
+    contract do
+      params do
+        required(:entity)
+      end
+    end
+
+    def call
+      entity = context.entity
+
+      context.object = EntityPresenter.call(
+        id: entity['_id'].to_i,
+        title: entity['_source']['title'],
+        intro: entity['_source']['intro'],
+        lookups: entity['_source']['lookups'].map(&:symbolize_keys),
+        kinds: entity['_source']['topics'].map(&:symbolize_keys),
+        images: entity['_source']['images'].map do |image|
+          {
+            id: image['id'],
+            image_url: ImageUploader::IMAGE_TYPES.include?(image['mime_type']) ? image['images']['200'] : nil,
+            video_url: ImageUploader::VIDEO_TYPES.include?(image['mime_type']) ? image['videos']['200'] : nil,
+            width: image['width'],
+            height: image['height'],
+            dark: image['dark']
+          }
+        end,
+        created_at: entity['_source']['created_at'],
+        entities_mentions_count: entity['_source']['entities_mentions_count'],
+        links: entity['_source']['link_url'].uniq.compact,
+        entity_url: Rails.application.routes.url_helpers.entity_path(
+          entity['_id'], host: GlobalHelper.host, locale: nil
+        )
+      ).object
+
+      context.object[:count] = context.count if context.count
+    end
+  end
+end
