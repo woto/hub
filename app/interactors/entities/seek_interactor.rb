2340293407e707bd6ae01015b622b323527ccffc
Ruslan Kornev <oganer@gmail.com>
2023-04-30 11:39:53 +0600

    refactor: use ElasticEntityPresenter in seek_interactor.rb

--- app/interactors/entities/seek_interactor.rb
+++ app/interactors/entities/seek_interactor.rb
@@ -21,29 +21,7 @@ module Entities
       @entities = GlobalHelper.elastic_client.search(query)
 
       context.object = @entities['hits']['hits'].map do |entity|
-        EntityPresenter.call(
-          id: entity['_id'],
-          title: entity['_source']['title'],
-          intro: entity['_source']['intro'],
-          lookups: entity['_source']['lookups'],
-          kinds: entity['_source']['topics'],
-          images: entity['_source']['images'].map do |image|
-            {
-              id: image['id'],
-              image_url: ImageUploader::IMAGE_TYPES.include?(image['mime_type']) ? image['images']['200'] : nil,
-              video_url: ImageUploader::VIDEO_TYPES.include?(image['mime_type']) ? image['videos']['200'] : nil,
-              width: image['width'],
-              height: image['height'],
-              dark: image['dark']
-            }
-          end,
-          created_at: entity['_source']['created_at'],
-          entities_mentions_count: entity['_source']['entities_mentions_count'],
-          entity_url: Rails.application.routes.url_helpers.entity_url(
-            entity['_id'], host: GlobalHelper.host, locale: nil
-          ),
-          links: entity['_source']['link_url'].uniq.compact
-        ).object
+        ElasticEntityPresenter.call(entity: entity).object
       end
     end
   end
