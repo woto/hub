199c2f23b7e786f39ebcfe0867a43dcbe2a16468
Ruslan Kornev <oganer@gmail.com>
2023-05-01 15:13:10 +0600

    feat: add static html for mention and entity on listing page

---
+++ spec/requests/listings_controller_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ListingsController do
+  let!(:entity) { create(:entity) }
+  let!(:mention) { create(:mention, entities: [entity], title: 'Mention title') }
+  let!(:favorites_item) { create(:favorites_item, ext_id: entity.id, kind: 'entities') }
+
+  before do
+    Mention.__elasticsearch__.refresh_index!
+  end
+
+  it 'includes static html of mention and entity' do
+    get listing_path(entity)
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include(mention_path(mention))
+    expect(response.body).to include('Mention title')
+    expect(response.body).to include(entity_path(entity))
+    expect(response.body).to include(entity.title)
+  end
+end
