25cb7ac7ecfc14088d38d41a7c4437f0c2b2e3c8
Ruslan Kornev <oganer@gmail.com>
2023-07-04 01:55:14 +0600

    fix: actualize db/structure.sql

--- db/migrate/20220715202229_update_entities_intro_to_default_value.rb
+++
@@ -1,8 +0,0 @@
-class UpdateEntitiesIntroToDefaultValue < ActiveRecord::Migration[6.1]
-  def change
-    # TODO: remove me later
-    Entity.where(intro: nil).find_each do |entity|
-      entity.update(intro: 'описание отсутствует')
-    end
-  end
-end
