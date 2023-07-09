304ed3bbfc59a47884b914fc049882c7c008e68c
Ruslan Kornev <oganer@gmail.com>
2023-07-10 02:22:15 +0600

    feat: replace net/http with faraday in GetChannelIdFromUrlInteractor

--- app/interactors/extractors/youtube_com/get_channel_id_from_url_interactor.rb
+++ app/interactors/extractors/youtube_com/get_channel_id_from_url_interactor.rb
@@ -6,11 +6,14 @@ module Extractors
       include ApplicationInteractor
 
       def call
-        uri = URI(context.url)
+        conn = Faraday.new do |faraday|
+          faraday.request :retry # retry transient failures
+          faraday.response :follow_redirects # follow redirects
+          faraday.response :raise_error # raises an exception if response is a 4xx or 5xx code
+        end
+        result = conn.get(context.url)
 
-        result = Net::HTTP.get(uri) # => String
-
-        chunks = result.split('channel_id=')
+        chunks = result.body.split('channel_id=')
         context.object = chunks[1][0...24]
       end
     end
