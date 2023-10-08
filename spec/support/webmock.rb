2f31d83dad1e3510d26cfbb753265dfba01a39e2
Ruslan Kornev <oganer@gmail.com>
2023-10-08 16:31:56 +0200

    feat: attempt to fix chrome in circle ci

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,1 +1,1 @@
-WebMock.disable_net_connect!(allow_localhost: true, allow: ['googlechromelabs.github.io', 'edgedl.me.gvt1.com'])
+WebMock.disable_net_connect!(allow_localhost: true, allow: ['googlechromelabs.github.io', 'edgedl.me.gvt1.com', 'chromedriver.storage.googleapis.com'])
