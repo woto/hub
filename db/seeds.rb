12370bafd4bc27e9bcf1bc2b998485f9a134064b
Ruslan Kornev <oganer@gmail.com>
2023-11-26 17:29:54 +0100

    fix: seeds in one invocation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@
 
 GlobalHelper.create_elastic_indexes
 
+# NOTE: without this invocation, we must run migrations and seeding
+# in separate executions. Otherwise, it will exit with an error due to
+# the cache of model's columns during migration
+User.reset_column_information
+
 User.create!(
   email: 'admin@example.com',
   password: 'password',
